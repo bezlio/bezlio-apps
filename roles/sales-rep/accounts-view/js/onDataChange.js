d47df3e691657c7f28d9814c389b0154ba614575
Adam Ellis <aellis@SaberlogicLLC.local>
2017-02-16 12:52:06 -0500

    AE

--- roles/sales-rep/accounts-view/js/onDataChange.js
+++ roles/sales-rep/accounts-view/js/onDataChange.js
@@ -2,7 +2,7 @@ define(["./customer.js"], function (customer) {
  
     function OnDataChange (bezl) {
         if (bezl.data.CustList) {
-
+            bezl.vars.loading = false;
         }
     }
   
