5506b6572ed2d7727793e37c7bba5ddb1432c243
Adam Ellis <aellis@SaberlogicLLC.local>
2017-02-16 13:04:26 -0500

    AE

--- roles/sales-rep/accounts-view/js/onDataChange.js
+++ roles/sales-rep/accounts-view/js/onDataChange.js
@@ -3,6 +3,12 @@ define(["./customer.js"], function (customer) {
     function OnDataChange (bezl) {
         if (bezl.data.CustList) {
             bezl.vars.loading = false;
+
+            // Create an AddressURL column with an encoded version of each Address
+            // so that it can be part of a Google Maps AddressURL
+            for (var i = 0; i < bezl.data.CustList.length; i++) {
+                bezl.data.CustList[i].AddressURL = encodeURI(bezl.data.CustList[i].Address);
+            };
         }
     }
   
