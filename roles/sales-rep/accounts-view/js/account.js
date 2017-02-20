547dae70aa145651063cb4ec14b2e87923b2c718
Adam Ellis <aellis@SaberlogicLLC.local>
2017-02-20 10:29:55 -0500

    AE

--- roles/sales-rep/accounts-view/js/account.js
+++ roles/sales-rep/accounts-view/js/account.js
@@ -13,6 +13,16 @@ define(function () {
                         { "Key": "EmailAddress", "Value": bezl.env.currentUser }
                     ] },0);
                 break;
+            case "AccountContacts":
+                bezl.vars.loadingContacts = true; 
+
+                // Pull in the accounts list for the logged in user
+                bezl.dataService.add('AccountContacts','brdb','sales-rep-queries','ExecuteQuery', { 
+                    "QueryName": "GetAccountContacts",
+                    "Parameters": [
+                        { "Key": "EmailAddress", "Value": bezl.env.currentUser }
+                    ] },0);
+                break;
             default:
                 break;
         }
--- roles/sales-rep/accounts-view/js/onStartup.js
+++ roles/sales-rep/accounts-view/js/onStartup.js
@@ -3,6 +3,7 @@ define(["./account.js"], function (account) {
     function OnStartup (bezl) {        
         // Initiate the call to refresh the customer list
         account.runQuery(bezl, 'Accounts');
+        account.runQuery(bezl, 'AccountContacts');
 
         // Determine the current position of the user
         if (navigator.geolocation) {
