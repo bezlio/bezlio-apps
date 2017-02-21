ce4feed1ce606d6ea3b5a8af95ced80314eb52e6
Adam Ellis <aellis@SaberlogicLLC.local>
2017-02-21 11:14:28 -0500

    AE

--- roles/sales-rep/crm-calls-view/js/onStartup.js
+++ roles/sales-rep/crm-calls-view/js/onStartup.js
@@ -12,6 +12,11 @@ define(["./account.js"], function (account) {
         account.runQuery(bezl, 'CRMCalls');
       }
 
+      // Also pull in the list of defined CRM call types.  This is expecting a plugin instance
+      // to be defined in BRDB named sales-rep-calltypes which points to a data source for call
+      // types
+      bezl.dataService.add('CallTypes','brdb','sales-rep-queries','ExecuteQuery', { "QueryName": "GetCallTypes" },0);
+
       $(".panel").on("selectAccount", function(event, param1) {
         bezl.vars.selectedAccount = param1;
         account.runQuery(bezl, 'CRMCalls');
