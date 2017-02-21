e97666cdeea27cdefd0b70a434f881314b96df9c
Adam Ellis <aellis@SaberlogicLLC.local>
2017-02-21 12:47:30 -0500

    AE

--- roles/sales-rep/crm-calls-view/js/account.js
+++ roles/sales-rep/crm-calls-view/js/account.js
@@ -24,7 +24,7 @@ define(function () {
         // query, detect the CRM platform (via what was specified on setConfig)
         // and route this request to the appropriate integration
         if (bezl.vars.config.Platform == "Epicor10" || bezl.vars.config.Platform == "Epicor905") {
-            require(['https://rawgit.com/bezlio/bezlio-apps/development/library/epicor/crm.js'], function(functions) {
+            require(['https://rawgit.com/bezlio/bezlio-apps/development/libraries/epicor/crm.js'], function(functions) {
                 functions.addNote(bezl
                                 , bezl.vars.config.Platform
                                 , bezl.vars.config.Connection
