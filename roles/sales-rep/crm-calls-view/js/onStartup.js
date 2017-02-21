6da2d4292b8f6d96bf8ba9c9876105e803e8fe1f
Adam Ellis <aellis@SaberlogicLLC.local>
2017-02-21 11:40:46 -0500

    AE

--- roles/sales-rep/crm-calls-view/js/onStartup.js
+++ roles/sales-rep/crm-calls-view/js/onStartup.js
@@ -3,13 +3,12 @@ define(["./account.js"], function (account) {
   function OnStartup (bezl) {        
       // Load the object for the selected customer from local storage into
       // a variable we can work with
-      bezl.vars.selectedAccount = {};
       if (typeof(Storage) !== "undefined" && localStorage.getItem("selectedAccount")) {
           bezl.vars.selectedAccount = JSON.parse(localStorage.getItem("selectedAccount"));
       }
 
       // Initiate the call to refresh the crm calls
-      if (bezl.vars.selectedAccount.ID != null) {
+      if (bezl.vars.selectedAccount != null) {
         account.runQuery(bezl, 'CRMCalls');
       }
 
