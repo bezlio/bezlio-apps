932897bd992e5308d75fd9f161aec42282ee9b7a
Adam Ellis <aellis@SaberlogicLLC.local>
2017-02-22 13:38:58 -0500

    AE

--- roles/sales-rep/crm-calls-view/js/onDataChange.js
+++ roles/sales-rep/crm-calls-view/js/onDataChange.js
@@ -4,6 +4,12 @@ define(function () {
         if (bezl.data.CRMCalls) {
             bezl.vars.loading = false;
         }
+
+        if (bezl.data.AddCRMCall) {
+            if (bezl.data.AddCRMCall.BOUpdError) {
+                bezl.notificationService.showCriticalError(JSON.stringify(bezl.data.AddCRMCall.BOUpdError));
+            }
+        }
     }
   
     return {
