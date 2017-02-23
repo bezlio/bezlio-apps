7741a3176f698975e5ef036d654e2747ec207c79
Adam Ellis <aellis@SaberlogicLLC.local>
2017-02-23 12:55:58 -0500

    AE

--- roles/sales-rep/tasks-view/js/task.js
+++ roles/sales-rep/tasks-view/js/task.js
@@ -11,12 +11,45 @@ define(function () {
                         { "Key": "EmailAddress", "Value": bezl.env.currentUser }
                     ] },0);
                 break;
+            case "Tasks":
+                bezl.vars.loading = true; 
+
+                // Pull in the accounts list for the logged in user
+                bezl.dataService.add('Tasks','brdb','sales-rep-queries','ExecuteQuery', { 
+                    "QueryName": "GetAccountTasks",
+                    "Parameters": [
+                        { "Key": "EmailAddress", "Value": bezl.env.currentUser },
+                        { "Key": "ID", "Value": bezl.vars.selectedAccount.ID }
+                    ] },0);
+                break;
             default:
                 break;
         }
     }
 
+    function Select(bezl, task) {
+        task.Selected = true;
+        // Mark the selected customer as selected
+        // for (var i = 0; i < bezl.vars.selectedAccount.Tasks.length; i++) {
+        //     if (bezl.data.Accounts[i].ID == account.ID) {
+        //         bezl.data.Accounts[i].Selected = !bezl.data.Accounts[i].Selected;
+
+        //         if (bezl.data.Accounts[i].Selected) {
+        //             localStorage.setItem('selectedAccount', JSON.stringify(bezl.data.Accounts[i]));
+        //             $('.panel').trigger('selectAccount', [bezl.data.Accounts[i]]);
+        //         } else {
+        //             localStorage.setItem('selectedAccount', '');
+        //             $('.panel').trigger('selectAccount', [{}]);
+        //         }
+                
+        //     } else {
+        //         bezl.data.Accounts[i].Selected = false;
+        //     }
+        // };
+    }
+
     return {
-        runQuery: RunQuery
+        runQuery: RunQuery,
+        select: Select
     }
 });
