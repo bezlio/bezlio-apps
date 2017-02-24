7bfc60500a20be67ad8fdc4d7e1108b766388121
Adam Ellis <aellis@SaberlogicLLC.local>
2017-02-24 16:38:53 -0500

    AE

--- roles/sales-rep/tasks-view/js/onDataChange.js
+++ roles/sales-rep/tasks-view/js/onDataChange.js
@@ -6,6 +6,16 @@ define(function () {
             bezl.vars.selectedAccount.Tasks = bezl.data.Tasks;
         }
 
+        if (bezl.data.TaskTypes) {
+            // Check to see if any tasks that were added before the task TaskTypes
+            // came back need one filled in
+            for (var i = 0; i < tasks.length; i++) {
+                if (tasks[i].RowState == 'Added' && tasks[i].TaskType == '') {
+                    tasks[i].TaskType[0].TaskType;
+                }
+            }
+        }
+
         if (bezl.data.UpdateTasks) {
             if (bezl.data.UpdateTasks.BOUpdError && bezl.data.UpdateTasks.BOUpdError.length > 0) {
                 bezl.notificationService.showCriticalError(JSON.stringify(bezl.data.UpdateTasks.BOUpdError));
