05c8e19ab5a74e13742df0eec0d9f5514a91a071
OnlyAGhost <bellis@saberlogic.com>
2017-02-27 16:36:12 -0500

    BE

--- libraries/epicor/order.js
+++ libraries/epicor/order.js
@@ -9,15 +9,20 @@ define(function () {
                       company,
                       custNum) {
         
-        bezl.dataService.add(
-            'NewOrder'
-            , 'brdb'
-            , 'sales-rep-newOrder'
-            , 'SalesOrder_NewOrderByCustomer'
-            , {
-                'CustNum': custNum
-            }
-            , 0);
+        // bezl.dataService.add(
+        //     'NewOrder'
+        //     , 'brdb'
+        //     , 'sales-rep-newOrder'
+        //     , 'SalesOrder_NewOrderByCustomer'
+        //     , {
+        //         'CustNum': custNum
+        //     }
+        //     , 0);
+        bezl.dataService.add('newOrder','brdb','Epicor10','SalesOrder_NewOrderByCustomer',
+        { "Connection":"Epicor 10 AE", 
+            "Company":"EPIC06", 
+            "CustNum": custNum,
+        },0);
 
         bezl.vars.newOrder = true;
 
