85031d60a31903c3af9ca17d738ced5434960f8c
OnlyAGhost <bellis@saberlogic.com>
2017-02-27 10:58:15 -0500

    BE

---
+++ libraries/epicor/order.js
@@ -0,0 +1,29 @@
+define(function () {
+    /**
+     * Returns a ds of a new order for the selected customer
+     * @param {Object[]} bezl - A reference to the calling Bezl
+     * @param {string} company - Company ID for the call
+     * @param {Number} custNum - The customer number to add this call for
+     */
+    function NewOrder (bezl,
+                      company,
+                      custNum) {
+
+        bezl.dataService.add(
+            'NewOrder'
+            , 'brdb'
+            , 'sales-rep-newOrder'
+            , 'ExecuteBOMethod'
+            , {
+                'Parameters': [{ 'Key': 'CustNum', 'Value': custNum }]
+            }
+            , 0);
+
+        bezl.vars.newOrder = true;
+
+    }
+
+    return {
+        newOrder: NewOrder
+    }
+});
