5d73bed46afe967a26daab884dcc386872a4cd13
Nick Owens <nowens@saberlogic.com>
2017-03-07 10:12:07 -0500

    refactor

--- roles/sales-rep/invoice-inquiry/js/onDataChange.js
+++ roles/sales-rep/invoice-inquiry/js/onDataChange.js
@@ -11,7 +11,43 @@ define(function () {
                 bezl.vars.selectedInvoice = JSON.parse(localStorage.getItem("selectedInvoice"));
             }
 
+            var tempInvoice = [];
             var tempLine = {};
+            bezl.vars.Invoices = new Array();
+            // Organize new object
+            for(var i = 0; i < bezl.data.Invoices.length; i++) {
+                // Invoice
+                tempInvoice.InvoiceNum = bezl.data.Invoices[i].InvoiceNum;
+                tempInvoice.InvoiceDate = bezl.data.Invoices[i].InvoiceDate;
+                tempInvoice.InvoiceAmt = bezl.data.Invoices[i].InvoiceAmt;
+                tempInvoice.InvoiceBal = bezl.data.Invoices[i].InvoiceBal;
+                tempInvoice.OrderDate = bezl.data.Invoices[i].OrderDate;
+                tempInvoice.PoNum = bezl.data.Invoices[i].PoNum;
+                tempInvoice.Company = bezl.data.Invoices[i].Company;
+
+                // Add a Selected property to the account record
+                if (bezl.data.Invoices[i].InvoiceNum == bezl.vars.selectedInvoice.InvoiceNum) {
+                    tempInvoice.Selected = true;
+                } else {
+                    tempInvoice.Selected = false;
+                }
+
+                // Line
+                tempLine.InvoiceLine = bezl.data.Invoices[i].InvoiceLine;
+                tempLine.PartNum = bezl.data.Invoices[i].PartNum;
+                tempLine.PartDescrition = bezl.data.Invoices[i].PartDescrition;
+                tempLine.Qty = bezl.data.Invoices[i].Qty;
+                tempLine.UnitPrice = bezl.data.Invoices[i].UnitPrice;
+                tempLine.ExtPrice = bezl.data.Invoices[i].ExtPrice;
+                // Push line into invoice
+                tempInvoice.InvoiceLines = new Array();
+                tempInvoice.InvoiceLines.push(tempLine); 
+
+                // Push invoice into final data var
+                bezl.vars.Invoices.push(tempInvoice); 
+            }
+
+            /*var tempLine = {};
             // Perform additional processing on the returned data
             for (var i = 0; i < bezl.data.Invoices.length; i++) {
                 // Add a Selected property to the account record
@@ -47,14 +83,15 @@ define(function () {
                 });
          // group invoices together
         var previousInvoice = {};
+        previousInvoice.InvoiceNum = 0;
          for (var i = 0; i < bezl.data.Invoices.length; i++) {
-                if (previousInvoice.InvoiceNum == bezl.date.Invoices[i].InvoiceNum) {
+                if (previousInvoice.InvoiceNum == bezl.data.Invoices[i].InvoiceNum) {
                     for (var j = 0; j < previousInvoice.InvoiceLines.length; j++){
                         bezl.data.Invoices[i].InvoicesLines.push(previousInvoice.InvoiceLines[j]);
                     }
                 } 
             } 
-         previousInvoice = bezl.date.Invoices[i];    
+         previousInvoice = bezl.date.Invoices[i];    */
 
     }
     }
