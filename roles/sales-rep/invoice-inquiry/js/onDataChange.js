9d7fa990c4862255a4a1e3cfb08549f845c5ca88
Nick Owens <nowens@saberlogic.com>
2017-03-07 11:54:33 -0500

    no

--- roles/sales-rep/invoice-inquiry/js/onDataChange.js
+++ roles/sales-rep/invoice-inquiry/js/onDataChange.js
@@ -23,39 +23,47 @@ define(function () {
 
                 // If invoice num already exist in new object, move invoice lines over
                 if (bezl.vars.Invoices.find(invoice => invoice.InvoiceNum == bezl.data.Invoices[i].InvoiceNum)) {
-                    console.log(bezl.data.Invoices[i].InvoiceNum);
-                }
-
-
-                // Invoice
-                tempInvoice.InvoiceNum = bezl.data.Invoices[i].InvoiceNum;
-                tempInvoice.InvoiceDate = bezl.data.Invoices[i].InvoiceDate;
-                tempInvoice.InvoiceAmt = bezl.data.Invoices[i].InvoiceAmt;
-                tempInvoice.InvoiceBal = bezl.data.Invoices[i].InvoiceBal;
-                tempInvoice.OrderDate = bezl.data.Invoices[i].OrderDate;
-                tempInvoice.PoNum = bezl.data.Invoices[i].PoNum;
-                tempInvoice.Company = bezl.data.Invoices[i].Company;
-
-                // Add a Selected property to the account record
-                if (bezl.data.Invoices[i].InvoiceNum == bezl.vars.selectedInvoice.InvoiceNum) {
-                    tempInvoice.Selected = true;
+                    // Line
+                    tempLine.InvoiceLine = bezl.data.Invoices[i].InvoiceLine;
+                    tempLine.PartNum = bezl.data.Invoices[i].PartNum;
+                    tempLine.PartDescription = bezl.data.Invoices[i].PartDescription;
+                    tempLine.Qty = bezl.data.Invoices[i].Qty;
+                    tempLine.UnitPrice = bezl.data.Invoices[i].UnitPrice;
+                    tempLine.ExtPrice = bezl.data.Invoices[i].ExtPrice;
+                    // Push line into invoice
+                    bezl.vars.Invoices[bezl.vars.Invoices.findIndex(inv => inv.InvoiceNum == bezl.data.Invoices[i].InvoiceNum)].push(tempLine);
                 } else {
-                    tempInvoice.Selected = false;
+
+                    // Invoice
+                    tempInvoice.InvoiceNum = bezl.data.Invoices[i].InvoiceNum;
+                    tempInvoice.InvoiceDate = bezl.data.Invoices[i].InvoiceDate;
+                    tempInvoice.InvoiceAmt = bezl.data.Invoices[i].InvoiceAmt;
+                    tempInvoice.InvoiceBal = bezl.data.Invoices[i].InvoiceBal;
+                    tempInvoice.OrderDate = bezl.data.Invoices[i].OrderDate;
+                    tempInvoice.PoNum = bezl.data.Invoices[i].PoNum;
+                    tempInvoice.Company = bezl.data.Invoices[i].Company;
+
+                    // Add a Selected property to the account record
+                    if (bezl.data.Invoices[i].InvoiceNum == bezl.vars.selectedInvoice.InvoiceNum) {
+                        tempInvoice.Selected = true;
+                    } else {
+                        tempInvoice.Selected = false;
+                    }
+
+                    // Line
+                    tempLine.InvoiceLine = bezl.data.Invoices[i].InvoiceLine;
+                    tempLine.PartNum = bezl.data.Invoices[i].PartNum;
+                    tempLine.PartDescription = bezl.data.Invoices[i].PartDescription;
+                    tempLine.Qty = bezl.data.Invoices[i].Qty;
+                    tempLine.UnitPrice = bezl.data.Invoices[i].UnitPrice;
+                    tempLine.ExtPrice = bezl.data.Invoices[i].ExtPrice;
+                    // Push line into invoice
+                    tempInvoice.InvoiceLines = new Array();
+                    tempInvoice.InvoiceLines.push(tempLine); 
+
+                    // Push invoice into final data var
+                    bezl.vars.Invoices.push(tempInvoice); 
                 }
-
-                // Line
-                tempLine.InvoiceLine = bezl.data.Invoices[i].InvoiceLine;
-                tempLine.PartNum = bezl.data.Invoices[i].PartNum;
-                tempLine.PartDescription = bezl.data.Invoices[i].PartDescription;
-                tempLine.Qty = bezl.data.Invoices[i].Qty;
-                tempLine.UnitPrice = bezl.data.Invoices[i].UnitPrice;
-                tempLine.ExtPrice = bezl.data.Invoices[i].ExtPrice;
-                // Push line into invoice
-                tempInvoice.InvoiceLines = new Array();
-                tempInvoice.InvoiceLines.push(tempLine); 
-
-                // Push invoice into final data var
-                bezl.vars.Invoices.push(tempInvoice); 
             }
 
             /*var tempLine = {};
