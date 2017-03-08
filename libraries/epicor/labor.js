1e5e2c47cbb2e5e7b3c863c50a2713e6f57ed94f
KCraneGH <KCrane@SaberLogic.com>
2017-03-08 16:16:39 -0500

    BezlExport.txt - Fixed weblink pathing. labor.js - Removed StartIndirect return having no matching function.

--- libraries/epicor/labor.js
+++ libraries/epicor/labor.js
@@ -126,7 +126,6 @@ define(function () {
         clockIn: ClockIn,
         clockOut: ClockOut,
         endActivities: EndActivities,
-        startJob: StartJob,
-        startIndirect: StartIndirect
+        startJob: StartJob
     }
 });
