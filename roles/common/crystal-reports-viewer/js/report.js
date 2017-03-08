84f749b774bf6875c548cc78ad0c87029964c1cd
Adam Ellis <aellis@SaberlogicLLC.local>
2017-03-08 09:04:20 -0500

    AE

--- roles/common/crystal-reports-viewer/js/report.js
+++ roles/common/crystal-reports-viewer/js/report.js
@@ -77,6 +77,7 @@ define(function () {
     return {
         runQuery: RunQuery,
         runReport: RunReport,
-        addParameterValue: AddParameterValue
+        addParameterValue: AddParameterValue,
+        back: Back
     }
 });
