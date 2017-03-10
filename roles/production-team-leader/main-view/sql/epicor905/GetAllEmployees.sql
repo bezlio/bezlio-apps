6205bbad981e8f23d54b2cfcc2699a65d94db847
KCraneGH <KCrane@SaberLogic.com>
2017-03-10 14:42:46 -0500

    Connection test continued.

--- roles/production-team-leader/main-view/sql/epicor905/GetAllEmployees.sql
+++ roles/production-team-leader/main-view/sql/epicor905/GetAllEmployees.sql
@@ -1,7 +1,7 @@
 SELECT
 	emp.EmpID
 	, emp.Name
-	, CASE WHEN lh.LaborHedSeq IS NULL THEN 0 ELSE 1 END AS ClockedInX
+	, CASE WHEN lh.LaborHedSeq IS NULL THEN 0 ELSE 1 END AS ClockedIn
 	, lh.LaborHedSeq AS LaborID
 	, CurrentActivity = (SELECT TOP 1 CAST(ld.JobNum AS VARCHAR) + '.' + CAST(ld.AssemblySeq AS VARCHAR) + '.' + CAST(ld.OprSeq AS VARCHAR) FROM LaborDtl ld WHERE ld.Company = lh.Company AND ld.LaborHedSeq = lh.LaborHedSeq AND ld.ActiveTrans = 1)
 
@@ -12,6 +12,3 @@ FROM
 	lh.Company = emp.Company
 	AND lh.EmployeeNum = emp.EmpID
 	AND lh.ActiveTrans = 1
-WHERE
-	emp.EmpStatus = 'A'
-	AND emp.Company = 'EPIC06'
