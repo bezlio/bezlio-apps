2abefc70d64851288dd417eb2a80dd4bd627469b
OnlyAGhost <bellis@saberlogic.com>
2017-03-13 16:00:38 -0400

    BE

--- roles/sales-rep/trip-planner/js/customer.js
+++ roles/sales-rep/trip-planner/js/customer.js
@@ -29,7 +29,7 @@ define(["./map.js"], function (map) {
             case "CustList":
                 // Pull in the customer list for the logged in user
                 bezl.dataService.add('CustList','brdb','sales-rep-queries','ExecuteQuery', { 
-                    "QueryName": "GetCustomersWithAddress",
+                    "QueryName": "/trip-planner/GetCustomersWithAddress",
                     "Parameters": [
                         { "Key": "EmailAddress", "Value": bezl.env.currentUser }
                     ] },0);
