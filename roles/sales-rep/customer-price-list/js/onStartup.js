98affe0f5a31e795e35bc2077d2d10af4fabb629
Nick Owens <nowens@saberlogic.com>
2017-03-14 10:22:00 -0400

    NO

--- roles/sales-rep/customer-price-list/js/onStartup.js
+++ roles/sales-rep/customer-price-list/js/onStartup.js
@@ -7,10 +7,6 @@ define(["./priceList.js"],function (priceList) {
         bezl.vars.sortCol = "";
         bezl.vars.filter = "";
 
-        $("input").keyup( function(){
-            priceList.filter();
-        });
-
         // If there was a previously selected customer in localStorage, grab a reference
             // so we can know whether to mark them as selected
             bezl.vars.selectedAccount  = {};
