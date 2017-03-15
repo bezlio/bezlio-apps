71838c930cd6d4ec12e6324c0f157d80ff06995c
OnlyAGhost <bellis@saberlogic.com>
2017-03-15 11:47:43 -0400

    BE

--- roles/customer-portal/order-entry/js/order.js
+++ roles/customer-portal/order-entry/js/order.js
@@ -2,40 +2,6 @@ define(["./customer.js"], function (customer) {
     function RunQuery (bezl, queryName) {
 
     }
-  
-    function Select(bezl, account) {
-        // Dropdown only allows single selecting
-        // Mark all of them as not selected
-        bezl.data.Customers.forEach(a => a.Selected = false);
-        bezl.vars.selectedShipTo = null;
-
-        // Select the one we selected
-        bezl.vars.selectedCustomer = bezl.data.Customers.find(a => a.ID == account.ID);
-        bezl.vars.selectedCustomer.Selected = true;
-
-        // Run our query to load parts
-        customer.RunQuery(bezl, 'GetPartsByCustNum');
-
-        // Load a new Order
-        NewOrder(bezl);
-
-        //localStorage.setItem('selectedAccount', JSON.stringify(bezl.vars.selectedAccount));
-        //$('.panel').trigger('selectAccount', [bezl.vars.selectedAccount]);
-
-        // Filter our contacts
-        bezl.vars.filteredContacts = bezl.data.CustomersContacts.filter(c => c.ID == account.ID);
-
-        // Filter out shiptos
-        bezl.vars.filteredShipTos = bezl.data.CustomersShipTos.filter(st => st.ID == account.ID);
-    }
-
-    function SelectShipTo(bezl, shipto) {
-        // Dropdown only allows single selecting
-        // Mark all of them as not selected
-        bezl.data.CustomersShipTos.forEach(a => a.Selected = false);
-        bezl.vars.selectedShipTo = bezl.data.CustomersShipTos.find(st => st.ID == shipto.ID && st.ShipToNum == shipto.ShipToNum);
-        bezl.vars.selectedShipTo.Selected = true;
-    }
 
     function AddLine (bezl) {
         bezl.vars.partList.push({
@@ -76,8 +42,6 @@ define(["./customer.js"], function (customer) {
 
     return {
         runQuery: RunQuery,
-        select: Select,
-        selectShipTo: SelectShipTo,
         addLine: AddLine,
         newOrder: NewOrder,
         submitOrder: SubmitOrder
