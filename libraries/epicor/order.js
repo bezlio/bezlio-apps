aa9e9392b8f04822650c1fa049629c4b93f14cc7
OnlyAGhost <bellis@saberlogic.com>
2017-03-16 16:38:59 -0400

    BE

--- libraries/epicor/order.js
+++ libraries/epicor/order.js
@@ -2,61 +2,48 @@ define(function () {
     /**
      * Returns a ds of a new order for the selected customer
      * @param {Object[]} bezl - A reference to the calling Bezl
-     * @param {string} company - Company ID for the call
-     * @param {Number} custNum - The customer number to add this call for
      */
-    function NewOrder (bezl,
-                      company,
-                      custNum) {
-        
-        // bezl.dataService.add(
-        //     'NewOrder'
-        //     , 'brdb'
-        //     , 'sales-rep-newOrder'
-        //     , 'SalesOrder_NewOrderByCustomer'
-        //     , {
-        //         'CustNum': custNum
-        //     }
-        //     , 0);
+    function NewOrder (bezl) {
+
         bezl.dataService.add('newOrder','brdb','Epicor10','SalesOrder_NewOrderByCustomer',
-        { "Connection":"Epicor 10 AE", 
-            "Company":"EPIC06", 
-            "CustNum": custNum,
+        { "Connection": bezl.vars.connection, 
+            "Company": bezl.vars.company, 
+            "CustNum": bezl.vars.selectedCustomer.CustNum,
         },0);
 
         bezl.vars.newOrder = true;
 
     }
 
-    function SubmitOrder (bezl, company) {
+    function SubmitOrder (bezl) {
         
         // This will take the structure from our bezl vars and stuff it into the ds specific to Epicor
         bezl.vars.partList.forEach(p => {
             bezl.vars.ds.OrderDtl.push({
                 OpenLine: true,
                 VoidLine: false,
-                Company: 'EPIC06',
+                Company: bezl.vars.company,
                 OrderNum: 0,
                 OrderLine: 0,
                 LineType: 'PART',
                 PartNum: p.PartNum,
                 LineDesc: p.PartDescription,
-                IUM: p.IUM,
+                IUM: p.UOM,
                 RevisionNum: '',
                 SellingQuantity: p.Qty,
                 UnitPrice: p.UnitPrice,
                 DocUnitPrice: p.UnitPrice,
                 MktgCampaignID: 'Customer',
                 MktgEvntSeq: 1,
-                CustNum: bezl.vars.selectedAccount.CustNum,
+                CustNum: bezl.vars.selectedCustomer.CustNum,
                 LockQty: false,
                 RowMod: 'U'
             })
         });
         // Now we will submit the order for processing
         bezl.dataService.add('submitOrder','brdb','Epicor10','SalesOrder_SubmitNewOrder',
-                            { "Connection":"Epicor 10 AE", 
-                            "Company":"EPIC06", 
+                            { "Connection": bezl.vars.connection, 
+                            "Company": bezl.vars.company, 
                             "ds": JSON.stringify(bezl.vars.ds)
                             },0);
 
