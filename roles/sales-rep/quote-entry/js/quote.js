a5f69c1493c01de6b4dce4a17ae7c47d46d5da94
Bob Schnable <rschnable@saberlogic.com>
2017-03-16 14:30:17 -0400

    add quote line;

--- roles/sales-rep/quote-entry/js/quote.js
+++ roles/sales-rep/quote-entry/js/quote.js
@@ -39,9 +39,16 @@ define(function () {
         };
     }
 
+    function AddLine(bezl) {
+        var lineNum = Math.max.apply(Math, bezl.vars.quoteData.quoteLines.map(function (dtl) { return dtl.QuoteLine; }));
+
+        bezl.vars.quoteData.quoteLines.push({ QuoteLine: lineNum, PartNum: '', Qty: 1, UOM: 'EA' });
+    }
+
     return {
         runQuery: RunQuery,
-        returnToSummary: ReturnToSummary
+        returnToSummary: ReturnToSummary,
+        addLine: AddLine
     }
 });
 
