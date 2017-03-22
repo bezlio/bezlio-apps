237c7dc435144e540898a54badce83d1710376f7
Nick Owens <nowens@saberlogic.com>
2017-03-22 09:34:32 -0400

    no

--- roles/common/geolocation/js/onDataChange.js
+++ roles/common/geolocation/js/onDataChange.js
@@ -27,7 +27,7 @@ define(["./customer.js",
                                         });
                                         
                 // Find how many address need geolocs
-                if(bezl.data.CustList[i].Geocode_Location != '' || bezl.data.CustList[i].Geocode_Location != null) {
+                if(bezl.data.CustList[i].Geocode_Location == '' || bezl.data.CustList[i].Geocode_Location == null) {
                     bezl.vars.geoLocsNeeded++;
                 }
             }
