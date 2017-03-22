f1c5575f4d91c187f2fede8a2c193a3fe1f07555
Nick Owens <nowens@saberlogic.com>
2017-03-22 12:47:39 -0400

    no

--- roles/common/geolocation/js/onDataChange.js
+++ roles/common/geolocation/js/onDataChange.js
@@ -1,113 +1,196 @@
-define(["./customer.js",
-        "./map.js"], function (customer, map) {
-            var firstTime = true;
- 
-    function OnDataChange (bezl) {
-        // Populate the 'customers' array if we got CustomerList back
-        if (bezl.data.CustList) {
+define(["./customer.js"], function (customer) {
 
-            // Reset Counters
-            bezl.vars.geoTracker = 0;
-            bezl.vars.geoLocsNeeded = 0;
-            bezl.vars.geoLocsDone = 0;
+    var delay = 100;
+    var nextAddress = 0;
 
-            //for testing.
-                if(firstTime) {
-                    for (var i = 0; i < bezl.data.CustList.length; i++) {
-                        bezl.data.CustList[i].Geocode_Location = '';
-                    }
-                firstTime = false;
-                }
-    
+    function GetInfoWindowContent (Title, Address, Contacts) {
+        // Develop the HTML for the customer contacts
+        var contactHtml = '<br><h4>Contacts</h4>';
+        var contacts = $.parseXML(Contacts);
+        var contactsXml = $(contacts);
 
-            bezl.vars.loading.customerList = true;
-            bezl.vars.customers = [];
-            for (var i = 0; i < bezl.data.CustList.length; i++) {
-                bezl.vars.customers.push({ selected: false,
-                                            key: bezl.data.CustList[i].CustNum,
-                                            display: bezl.data.CustList[i].Name,
-                                            lastContact: (bezl.data.CustList[i].LastContact || 'T').split('T')[0],
-                                            nextTaskDue: (bezl.data.CustList[i].NextTaskDue || 'T').split('T')[0],
-                                            distance: null,
-                                            streetAddress: bezl.data.CustList[i].Address, 
-                                            title: bezl.data.CustList[i].Name, 
-                                            custNum: bezl.data.CustList[i].CustNum,
-                                            shipToNum: bezl.data.CustList[i].ShipToNum,
-                                            data: bezl.data.CustList[i]
-                                        });
+        contactsXml.find('Contact').each(function(index){
+            var cntName = $(this).find('ContactName').text();
+            var cntEmail = $(this).find('EMailAddress').text();
+            var title = $(this).find('ContactTitle').text();
+            var phoneNum = $(this).find('PhoneNum').text();
 
-                
-                                        
-                // Find how many address need geolocs
-                if(bezl.data.CustList[i].Geocode_Location == '' || bezl.data.CustList[i].Geocode_Location == null) {
-                    bezl.vars.geoLocsNeeded++;
-                }
+            if (title) {
+            contactHtml += '<b>' + title + '</b>';
             }
+
+            if (cntName) {
+            contactHtml += '<br>' + cntName;
+            }
+
+            if (phoneNum) {
+            contactHtml += '<br>Phone: <a href=\"tel:' + phoneNum + '\">' + phoneNum + '</a>';
+            }
+
+            if (cntEmail) {
+            contactHtml += '<br>Email: <a href=\"mailto:' + cntEmail + '\">' + cntEmail + '</a>';
+            }
+
+            contactHtml += '<br>';
+        });
+
+        var contentString = 
+            '<div id="content">'+
+                '<div id="siteNotice">'+
+                '</div>'+
+                '<h4 id="firstHeading" class="firstHeading">' + Title + '</h4>'+
+                '<div id="bodyContent">'+
+                    '<a href=\"http://maps.google.com/maps?q=' + encodeURI(Address) + '\" target=\"_blank\">' + Address + '</a>' +
+                    ((contacts) ? contactHtml : '')
+                '</div>'+
+            '</div>';
         
-            // Configure the typeahead controls for the customer and customer search.  For full documentation of
-            // available settings here see http://www.runningcoder.org/jquerytypeahead/documentation/
-            /*$('.js-typeahead-customers').typeahead({
-                order: "asc",
-                maxItem: 8,
-                source: {
-                    data: function() { return bezl.vars.customers; }
-                },
-                callback: {
-                    onClick: function (node, a, item, event) {
-                        customer.select(bezl, item.key);
-                    }
-                }
-            });
-        
-            $('.js-typeahead-customers2').typeahead({
-                order: "asc",
-                maxItem: 8,
-                source: {
-                    data: function() { return bezl.vars.customers; }
-                },
-                callback: {
-                    onClick: function (node, a, item, event) {
-                        customer.select(bezl, item);
-                    }
-                }
-            });
-                    
-            bezl.vars.loading.customerList = false;*/
+        return contentString;
+    }
 
-             // Now loop through the results and plot each
-            /*for (var i = 0; i < bezl.data.CustList.length; i++) {
-                if (bezl.data.CustList[i].Address.length > 3) {
-                
-                    // Test to see whether we already saved the geocode.  If not, use the API to calculate it and save it
-                    if (bezl.data.CustList[i].Geocode_Location == "" ||  bezl.data.CustList[i].Geocode_Location == null) {
-                                           
-                    } else {
-                        var marker = new bezl.vars.client.Marker({
-                            position: { lat: + parseFloat(bezl.data.CustList[i].Geocode_Location.split(',')[0].split(':')[1]), lng: parseFloat(bezl.data.CustList[i].Geocode_Location.split(',')[1].split(':')[1]) },
-                            map: bezl.vars.map,
-                            title: bezl.data.CustList[i].Name,
-                            data: bezl.data.CustList[i],
-                            lat: parseFloat(bezl.data.CustList[i].Geocode_Location.split(',')[0].split(':')[1]),
-                            lng: parseFloat(bezl.data.CustList[i].Geocode_Location.split(',')[1].split(':')[1])
-                        });
-                        
-                        bezl.vars.markers[bezl.data.CustList[i].CustNum] = marker;
-                    }
-                }
-            };*/
+     function theNext(bezl) {
+        if(bezl.vars.geoLocsNeeded > 0) {
+        if (nextAddress < bezl.vars.geoLocsNeeded) {
+            console.log(bezl.vars.customers[nextAddress]);
+            if(bezl.vars.customers[nextAddress]){
+            if(bezl.vars.customers[nextAddress].data.Geocode_Location == '' || bezl.vars.customers[nextAddress].data.Geocode_Location == null)
+            {
+                setTimeout(function(){getAddress({ 
+                                    streetAddress: bezl.vars.customers[nextAddress].data.Address, 
+                                    title: bezl.vars.customers[nextAddress].data.Name, 
+                                    custNum: bezl.vars.customers[nextAddress].data.CustNum,
+                                    shipToNum: bezl.vars.customers[nextAddress].data.ShipToNum,
+                                    data: bezl.vars.customers[nextAddress].data 
+                                }, theNext, bezl)}, delay);
+            }
+            }
+          nextAddress++;
+        } else {
+            // If no customers needed geocoding, do not run again.
+            if(bezl.vars.geoLocsNeeded != 0) {
             
+                nextAddress = 0;
+                // Run Query to update results. Acts as second check as well
+                bezl.functions["getCustomers"]();
+            } else {
 
-            //Geocode each address that doesnt have a geocode.
-            map.theNext(bezl);
-
-            // Clean up CustList data subscription as we no longer need it
-            bezl.dataService.remove('CustList');
-            bezl.data.CustList = null;
-            
+                // We're done. 
+                bezl.vars.geoTracker = 100;
+                var progressBar = document.getElementById('geoProgress');
+                // Forces update of DOM
+                    setTimeout(function(){ 
+                        progressBar.style.width= bezl.vars.geoTracker + '%';
+                    }, 200);
+                nextAddress = 0;
+                bezl.vars.loading.customerList = false;
+            }
         }
+    } else {
+          // We're done. 
+         bezl.vars.geoTracker = 100;
+         var progressBar = document.getElementById('geoProgress');
+         // Forces update of DOM
+            setTimeout(function(){ 
+                progressBar.style.width= bezl.vars.geoTracker + '%';
+            }, 200);
+            nextAddress = 0;
+            bezl.vars.loading.customerList = false;
+        }
+    
+      }
+
+      // ====== Geocoding ======
+      function getAddress(customerRecord, next, bezl) {
+        bezl.vars.geocoder.geocode({address:`${customerRecord.streetAddress}`}, function (results,status)
+          { 
+            // If that was successful
+            if (status == google.maps.GeocoderStatus.OK) {
+              // Output the data
+               if (results != null && results.length > 0) {
+
+                var marker = new bezl.vars.client.Marker({
+                position: results[0].geometry.location,
+                map: bezl.vars.map,
+                title: customerRecord.title,
+                data: customerRecord.data,
+                lat: results[0].geometry.location.lat(),
+                lng: results[0].geometry.location.lng()
+            });
+               }
+             bezl.vars.markers[customerRecord.custNum] = marker;
+
+
+              var g;
+              if( results[0].geometry.location == '' || results[0].geometry.location == null ){
+                  g = 'Unavailable';
+              } else {
+                g = JSON.stringify(results[0].geometry.location)
+                .replace(/"/g, '')
+                .replace('{', '')
+                .replace('}', '');
+              }
+
+            bezl.vars.geoLocsDone++;
+            bezl.vars.geoTracker = Math.round((bezl.vars.geoLocsDone / bezl.vars.geoLocsNeeded)*100);
+            var progressBar = document.getElementById('geoProgress');
+
+            // Forces update of DOM
+            setTimeout(function(){ 
+                progressBar.style.width= bezl.vars.geoTracker + '%';
+            }, 200);
+
+
+            updateGeo(bezl, customerRecord, g);
+
+                
+            }
+            else {
+              // if we were sending the requests to fast, try this one again and increase the delay
+              if (status == google.maps.GeocoderStatus.OVER_QUERY_LIMIT) {
+                nextAddress--;
+                delay++;
+              } else {
+                console.log('Geocoding Error');
+              }   
+            }
+            next(bezl);
+          }
+        );
+      }
+
+    function updateGeo(bezl, customerRecord, g) {
+         // Update the database so we don't need to look this up next time
+            bezl.dataService.add('SetGeocodeOnAddress_' + customerRecord.custNum,'brdb','sales-rep-queries','ExecuteNonQuery',
+                                { "QueryName": "SetGeocodeOnAddress",
+                                    "Parameters": [
+                                        { "Key": "Geocode_Location", "Value": g || '' },
+                                        { "Key": "CustNum", "Value": customerRecord.custNum },
+                                        { "Key": "CustID", "Value": customerRecord.data.CustID || '' }
+                                    ] },0);  
+    }
+
+    function UpdateAddress(bezl) {
+       
+        
+            bezl.vars.markers[nextAddress].setMap(null);
+            
+            var marker = new bezl.vars.client.Marker({
+                position: bezl.vars.customers[nextAddress].Geocode_Location,
+                map: bezl.vars.map,
+                label: 'A',
+                title: '',
+                lat: bezl.vars.customers[nextAddress].Geocode_Location.geometry.location.lat(),
+                lng: bezl.vars.customers[nextAddress].Geocode_Location.geometry.location.lng()
+            });
+              
+
+            bezl.vars.markers[nextAddress] = marker;
+            
     }
   
     return {
-        onDataChange: OnDataChange
+        getInfoWindowContent: GetInfoWindowContent,
+        updateAddress: UpdateAddress,
+        theNext: theNext
     }
 });
