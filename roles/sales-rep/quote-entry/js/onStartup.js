48f4a4057785fa0992878f8caa8fac4e98fec5f5
Bob Schnable <rschnable@saberlogic.com>
2017-03-23 15:09:54 -0400

    RS

--- roles/sales-rep/quote-entry/js/onStartup.js
+++ roles/sales-rep/quote-entry/js/onStartup.js
@@ -8,8 +8,15 @@ define(["./quote.js"], function (quote) {
         bezl.vars.ds.QuoteHed = [];
         bezl.vars.ds.QuoteDtl = [];
 
-        var parts = http.get('https://rawgit.com/bezlio/bezlio-apps/Sales-Rep---Request-For-Quote-Entry-%2332/roles/sales-rep/quote-entry/json/Part.json', '', '')
-            .map(result => result.json());
+        // var parts = http.get('https://rawgit.com/bezlio/bezlio-apps/Sales-Rep---Request-For-Quote-Entry-%2332/roles/sales-rep/quote-entry/json/Part.json', '', '')
+        //     .map(result => result.json());
+
+        require(["https://rawgit.com/bezlio/bezlio-apps/Sales-Rep---Request-For-Quote-Entry-%2332/roles/sales-rep/quote-entry/json/Part.json?callback=define"],
+            function (data) {
+                //The data object will be the API response for the
+                //JSONP data call.
+                console.log(data);
+            });
     }
 
     return {
