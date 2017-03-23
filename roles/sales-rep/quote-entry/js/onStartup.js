3edda3348bc233840cf7e979550148377e6a62ee
Bob Schnable <rschnable@saberlogic.com>
2017-03-23 15:36:29 -0400

    RS

--- roles/sales-rep/quote-entry/js/onStartup.js
+++ roles/sales-rep/quote-entry/js/onStartup.js
@@ -17,6 +17,9 @@ define(["./quote.js"], function (quote) {
         //         //JSONP data call.
         //         console.log(data);
         //     });
+
+        $.ajaxSetup({ dataType: "jsonp" });
+
         console.log('test');
         $.getJSON("https://rawgit.com/bezlio/bezlio-apps/Sales-Rep---Request-For-Quote-Entry-%2332/roles/sales-rep/quote-entry/json/Part.json", function (data) {
             console.log(data);
