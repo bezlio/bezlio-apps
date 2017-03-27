abd4d1991790679a91ee99dcc6da3b2b14d5b0c0
OnlyAGhost <bellis@saberlogic.com>
2017-03-27 14:34:26 -0400

    BE

--- roles/customer-portal/order-entry/js/order.js
+++ roles/customer-portal/order-entry/js/order.js
@@ -40,7 +40,7 @@ define(["./customer.js"], function (customer) {
         // query, detect the CRM platform (via what was specified on setConfig)
         // and route this request to the appropriate integration
         if (bezl.vars.Platform == "Epicor10" || bezl.vars.Platform == "Epicor905") {
-            require(['https://cdn.rawgit.com/bezlio/bezlio-apps/1.6/libraries/epicor/order.js'], function(functions) {
+            require(['https://rawgit.com/bezlio/bezlio-apps/development/libraries/epicor/order.js'], function(functions) {
                 functions.newOrder(bezl);
             }); 
         }
@@ -89,7 +89,7 @@ define(["./customer.js"], function (customer) {
         // query, detect the CRM platform (via what was specified on setConfig)
         // and route this request to the appropriate integration
         if (bezl.vars.Platform == "Epicor10" || bezl.vars.Platform == "Epicor905") {
-            require(['https://cdn.rawgit.com/bezlio/bezlio-apps/1.6/libraries/epicor/order.js'], function(functions) {
+            require(['https://rawgit.com/bezlio/bezlio-apps/development/libraries/epicor/order.js'], function(functions) {
                 functions.submitOrder(bezl);
             }); 
         }
