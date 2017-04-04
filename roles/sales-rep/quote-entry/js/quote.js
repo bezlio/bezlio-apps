444d80955385d7a6a81a04dd4f1eabd052b2afe6
Bob Schnable <rschnable@saberlogic.com>
2017-04-04 15:59:36 -0400

    RS

--- roles/sales-rep/quote-entry/js/quote.js
+++ roles/sales-rep/quote-entry/js/quote.js
@@ -48,9 +48,7 @@ define(function () {
         //bezl.data.QuoteDtls = bezl.data.QuoteDtls.filter(dtl => dtl.Deleted === 0);
 
         var lineNum = Math.max.apply(Math, bezl.data.QuoteDtls.map(function (dtl) { return dtl.QuoteLine; }));
-        lineNum = (lineNum === -Infinity) ? 1 : lineNum;
-
-        console.log(lineNum);
+        lineNum = (lineNum === -Infinity) ? 0 : lineNum;
 
         bezl.data.QuoteDtls.push({ QuoteLine: lineNum + 1, PartNum: '', OrderQty: 1, SellingExpectedUM: 'EA', Deleted: 0 });
 
