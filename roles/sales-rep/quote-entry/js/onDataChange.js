4545e32a9f9b3dcb6069baf20cea16c8310b2e32
Bob Schnable <rschnable@saberlogic.com>
2017-04-04 15:05:12 -0400

    RS

--- roles/sales-rep/quote-entry/js/onDataChange.js
+++ roles/sales-rep/quote-entry/js/onDataChange.js
@@ -7,41 +7,31 @@ define(function () {
         }
 
         if (bezl.data.QuoteDtls) {
-            //console.log('true - details exist');
-            bezl.vars.linesloading = false;
+            if (bezl.data.QuoteDtls !== undefined) {
 
-            // bezl.data.QuoteDtls.forEach(dtl => {
-            //     console.log(dtl);
-            // });
+                bezl.vars.linesloading = false;
 
-            // bezl.data.QuoteDtls = bezl.data.QuoteDtls.filter(dtl => dtl.Deleted === 0);
-
-            // bezl.data.QuoteDtls.forEach(dtl => {
-            //     console.log(dtl);
-            // });
-
-            //var partList = [{ "PartNum": "Server1" }, { "PartNum": "Server2" }, { "PartNum": "Server3" }]
-
-            bezl.data.QuoteDtls.map(dtl => {
-                var typeAhead = function (lineNum) {
-                    $(bezl.container.nativeElement).find(".js-typeahead-parts" + lineNum).typeahead('destroy');
-                    $(bezl.container.nativeElement).find(".js-typeahead-parts" + lineNum).typeahead({
-                        order: "asc",
-                        maxItem: 8,
-                        display: ['PART_DESCRIPTION', 'PART_ID'],
-                        source: {
-                            data: function () { return bezl.vars.parts; }
-                        },
-                        callback: {
-                            onClick: function (node, a, item, event) {
-                                console.log("test");
+                bezl.data.QuoteDtls.map(dtl => {
+                    var typeAhead = function (lineNum) {
+                        $(bezl.container.nativeElement).find(".js-typeahead-parts" + lineNum).typeahead('destroy');
+                        $(bezl.container.nativeElement).find(".js-typeahead-parts" + lineNum).typeahead({
+                            order: "asc",
+                            maxItem: 8,
+                            display: ['PART_DESCRIPTION', 'PART_ID'],
+                            source: {
+                                data: function () { return bezl.vars.parts; }
+                            },
+                            callback: {
+                                onClick: function (node, a, item, event) {
+                                    console.log("test");
+                                }
                             }
-                        }
-                    });
-                }
+                        });
+                    }
 
-                setTimeout(typeAhead, 2, dtl.QuoteLine);
-            });
+                    setTimeout(typeAhead, 2, dtl.QuoteLine);
+                });
+            }
         }
 
         if (bezl.data.newQuote && bezl.data.Quotes && !bezl.vars.editingQuote) {
