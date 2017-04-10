9d2df87157c84f01cf0e0759079ee7b9ac48bc77
Bob Schnable <rschnable@saberlogic.com>
2017-04-10 15:15:00 -0400

    RS

--- roles/sales-rep/quote-entry/js/quote.js
+++ roles/sales-rep/quote-entry/js/quote.js
@@ -110,7 +110,8 @@ define(function () {
             curLine.Attributes = [];
 
             filterArray.forEach(attr => {
-                var attrFnd = JSON.parse(JSON.stringify(bezl.vars.attributes.find(attribute => attribute.ATTRIBUTE_ID === attr.ATTRIBUTE_ID)));
+                //var attrFnd = JSON.parse(JSON.stringify(bezl.vars.attributes.find(attribute => attribute.ATTRIBUTE_ID === attr.ATTRIBUTE_ID)));
+                var attrFnd = jQuery.extend(true, {}, bezl.vars.attributes.find(attribute => attribute.ATTRIBUTE_ID === attr.ATTRIBUTE_ID));
 
                 attrFnd.Display = false;
                 attrFnd.QuoteNum = curLine.QuoteNum;
