1a115b34525a9a4650461c4bdbbeea434bd4c243
KCraneGH <KCrane@SaberLogic.com>
2017-04-12 16:08:40 -0400

    Blind SQL conversion.

---
+++ roles/sales-rep/invoice-inquiry/sql/Prophet21/InvoiceInquiry.sql
@@ -0,0 +1,20 @@
+Select 
+	p21_view_invoice_hdr.invoice_no As InvoiceNum,
+	p21_view_invoice_hdr.invoice_date As InvoiceDate,
+	p21_view_invoice_hdr.total_amount As InvoiceAmt,
+	p21_view_invoice_hdr.total_amount - p21_view_invoice_hdr.amount_paid As InvoiceBal,
+	p21_view_invoice_hdr.order_date As OrderDate,
+	p21_view_invoice_hdr.po_no As PoNum,
+	p21_view_invoice_line.line_no As InvoiceLine,
+	p21_view_invoice_line.customer_part_number As PartNum,
+	p21_view_invoice_line.unit_price As UnitPrice,
+	p21_view_invoice_line.extended_price As ExtPrice,
+	p21_view_invoice_line.item_desc As PartDescription,
+	p21_view_invoice_line.SellingShipQty As Qty
+FROM
+	p21_view_invoice_line with (nolock)
+	INNER JOIN p21_view_invoice_hdr with (nolock) ON p21_view_invoice_line.company_id = p21_view_invoice_hdr.company_no  AND p21_view_invoice_line.invoice_no = p21_view_invoice_hdr.invoice_no
+WHERE 
+	p21_view_invoice_hdr.invoice_date >= '{StartDate}' AND p21_view_invoice_hdr.invoice_date <= '{EndDate}'
+ORDER BY 
+	p21_view_invoice_hdr.invoice_date Desc
