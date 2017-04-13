dd340833ceb8c6e2e22f63e341549df2d9cfe1ef
Nick Owens <nowens@saberlogic.com>
2017-04-13 14:25:02 -0400

    no

--- roles/sales-rep/invoice-inquiry/js/onStartup.js
+++ roles/sales-rep/invoice-inquiry/js/onStartup.js
@@ -1,14 +1,6 @@
 define(function () {
     function OnStartup(bezl) {
-
-      var endDate= new Date();
-      var startDate= new Date().setDate(endDate.getDate()-30);
-
-      console.log(endDate);
-      console.log(startDate);
-
-        bezl.vars.startDate = startDate.split('T')[0];
-        bezl.vars.endDate = new Date(endDate).split('T')[0];
+        
         bezl.vars.sort = "";
         bezl.vars.sortCol = "";
         bezl.vars.sortInner = "";
