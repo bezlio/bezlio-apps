9af93ee8220dfb24a4fd37d68b197cce15d7e12c
Bob Schnable <rschnable@saberlogic.com>
2017-04-27 11:19:12 -0400

    RS

--- libraries/epicor/quote.js
+++ libraries/epicor/quote.js
@@ -106,8 +106,7 @@ define(function () {
                 });
 
                 dtl.Attributes.map(attr => {
-                    console.log(attr);
-                    console.log(attr.ATTRIBUTE_VALUES.find(val => val.ATTRIBUTE_VALUE === 'OTHER'));
+                    var otherValue = (attr.ATTRIBUTE_VALUES.find(val => val.ATTRIBUTE_VALUE === 'OTHER') !== undefined) ? attr.ATTRIBUTE_VALUES.find(val => val.ATTRIBUTE_VALUE === 'OTHER').SELECTED_VALUE : '';
                     if (attr.hasOwnProperty("SELECTED_VALUE")) {
                         bezl.dataService.add('QuoteAttrs', 'brdb', 'sales-rep-queries', 'ExecuteNonQuery', {
                             "QueryName": "InsertAttributes",
@@ -118,7 +117,7 @@ define(function () {
                                 { Key: "AttributeID", Value: attr.ATTRIBUTE_ID },
                                 { Key: "ParentID", Value: '' },
                                 { Key: "AttributeValue", Value: (attr.SELECTED_VALUE === 'OTHER') ? 'OTHER' : attr.SELECTED_VALUE },
-                                { Key: "OtherAttributeValue", Value: (attr.ATTRIBUTE_VALUES.find(val => val.ATTRIBUTE_VALUE === 'OTHER') !== undefined) ? attr.ATTRIBUTE_VALUES.find(val => val.ATTRIBUTE_VALUE === 'OTHER').SELECTED_VALUE : 'N/A' },
+                                { Key: "OtherAttributeValue", Value: otherValue },
                                 { Key: "AttributeDesc", Value: attr.ATTRIBUTE_DESCRIPTION },
                                 { Key: "PartNum", Value: dtl.PartNum }
                             ]
