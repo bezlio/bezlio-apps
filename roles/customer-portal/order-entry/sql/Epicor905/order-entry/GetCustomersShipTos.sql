5b97a9d1b73e54c7b9c939019ee8c8088c621cf6
Nick Owens <nowens@saberlogic.com>
2017-04-27 15:28:02 -0400

    Added epicor905 queries

---
+++ roles/customer-portal/order-entry/sql/Epicor905/order-entry/GetCustomersShipTos.sql
@@ -0,0 +1,26 @@
+SELECT
+	c.CustID AS ID
+	, st.CustNum
+	, st.ShipToNum AS ShipToNum
+	, st.Name AS Name
+	, st.Address1 AS Address1
+	, st.Address2 AS Address2
+	, st.Address3 AS Address3
+	, st.City AS City
+	, st.State AS State
+	, st.Zip AS Zip
+	, st.PhoneNum AS PhoneNum
+FROM 
+	ShipTo st with(nolock)
+	
+	INNER JOIN Customer c with(nolock) ON
+	c.Company = st.Company
+	AND c.CustNum = st.CustNum
+	
+	INNER JOIN CustCnt cnt with(nolock) ON
+	cnt.Company = c.Company
+	AND cnt.CustNum = c.CustNum
+WHERE
+	c.Company = 'EPIC06'
+	--AND cnt.EMailAddress = 'administrator@wfo.epicor.com'
+	AND cnt.EMailAddress = '{EmailAddress}'
