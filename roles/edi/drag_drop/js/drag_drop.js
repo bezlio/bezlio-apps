375ef8d4fa8a5d8deb8f3f308a06703b895e159b
tmezzolestaSL <tmezzolesta@SaberlogicLLC.local>
2017-04-28 11:17:06 -0400

    added drag over function

--- roles/edi/drag_drop/js/drag_drop.js
+++ roles/edi/drag_drop/js/drag_drop.js
@@ -9,8 +9,29 @@ define(function () {
         //add advnaced upload to class
         var $form = $('.box');
 
-        if (isAdvancedUpload) {
-        $form.addClass('has-advanced-upload');
+        if (isAdvancedUpload) 
+        {
+            $form.addClass('has-advanced-upload');
+        }
+
+        if (isAdvancedUpload) 
+        {
+            var droppedFiles = false;
+
+            $form.on('drag dragstart dragend dragover dragenter dragleave drop', function(e) {
+                e.preventDefault();
+                e.stopPropagation();
+            })
+            .on('dragover dragenter', function() {
+                $form.addClass('is-dragover');
+            })
+            .on('dragleave dragend drop', function() {
+                $form.removeClass('is-dragover');
+            })
+            .on('drop', function(e) {
+                droppedFiles = e.originalEvent.dataTransfer.files;
+            });
+
         } 
     }
     return {
