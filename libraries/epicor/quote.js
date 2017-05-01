b222ae9ece3038456f513e2204f7a4f39f3cf0fd
Bob Schnable <rschnable@saberlogic.com>
2017-05-01 17:02:54 -0400

    RS

--- libraries/epicor/quote.js
+++ libraries/epicor/quote.js
@@ -131,6 +131,7 @@ define(function () {
                         //true or false attr values
                         var attrValsE = JSON.parse(JSON.stringify(attr.ATTRIBUTE_VALUES.filter(val => val.hasOwnProperty('EDITABLE') === false)));
                         attrValsE.map(val => {
+                            console.log(val);
                             let name = val.ATTRIBUTE_VALUE_LABEL.substring(0, 5).replace(" ", "");
                             bezl.dataService.add('QuoteAttrs_' + name, 'brdb', 'sales-rep-queries', 'ExecuteNonQuery', {
                                 "QueryName": "InsertAttributes",
