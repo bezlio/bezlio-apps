a15da52483543d1768399ec6d43698254feb1e0b
Bob Schnable <rschnable@saberlogic.com>
2017-05-01 11:06:36 -0400

    RS

--- roles/sales-rep/quote-entry/js/onDataChange.js
+++ roles/sales-rep/quote-entry/js/onDataChange.js
@@ -84,7 +84,7 @@ define(function () {
             bezl.dataService.remove('Attributes');
         }
 
-        if (bezl.data.find(data => data.indexOf('QuoteAttrs_'))) {
+        if (bezl.data.filter(data => data.indexOf('QuoteAttrs_').length > 0)) {
             console.log(bezl.data);
         }
 
