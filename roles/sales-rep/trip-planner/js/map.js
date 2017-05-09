560e97c7f810dbbef1ae243dea00d18c1c8770bd
Nick Owens <nowens@saberlogic.com>
2017-05-09 14:09:08 -0400

    no

--- roles/sales-rep/trip-planner/js/map.js
+++ roles/sales-rep/trip-planner/js/map.js
@@ -112,7 +112,7 @@ define([], function () {
                 '<h4 id="firstHeading" class="firstHeading">' + Title + '</h4>'+
                 '<div id="bodyContent">'+
                     '<a href=\"http://maps.google.com/maps?q=' + encodeURI(Address) + '\" target=\"_blank\">' + Address + '</a>' +
-                    `<div align="center" style="margin-top: 7px;"><button onclick="$(\''#bezlpanel'\').trigger(\''addCust_Pin'\',[]);" class="btn btn-sm btn-primary">+ Add to Route</button></div>` + 
+                    `<div align="center" style="margin-top: 7px;"><button onclick="$(\'#bezlpanel\').trigger(\'addCust_Pin\',[]);" class="btn btn-sm btn-primary">+ Add to Route</button></div>` + 
                     ((contacts) ? contactHtml : '') //onclick="$('#bezlpanel').trigger('addCust_Pin',[]);}"
                 '</div>'+
             '</div>';
