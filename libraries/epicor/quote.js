b731dafa153aeb9e2055b25304a7ad37a1691e4e
Bob Schnable <rschnable@saberlogic.com>
2017-05-10 09:58:40 -0400

    RS

--- libraries/epicor/quote.js
+++ libraries/epicor/quote.js
@@ -170,31 +170,31 @@ define(function () {
                     }
 
                     //sub attributes
-                    console.log(attr);
-                    console.log(attr.SELECTED_VALUE);
-                    var x = attr.ATTRIBUTE_VALUES.find(attrVal_subAttr => attrVal_subAttr.ATTRIBUTE_VALUE === attr.SELECTED_VALUE);
-                    console.log(x);
-                    //if (attr.ATTRIBUTE_VALUES.find(attrVal => attrVal.ATTRIBUTE_VALUE === attr.SELECTED_VALUE).hasOwnProperty("SUB_ATTRIBUTE")) {
-                    //  console.log(attr);
-                    // attr.ATTRIBUTE_VALUES.SUB_ATTRIBUTE.map(subAttr => {
-                    //     subAttr.ATTRIBUTE_VALUES.map(subAttrVal => {
-                    //         bezl.dataService.add('QuoteAttrs_Sub', 'brdb', 'sales-rep-queries', 'ExecuteNonQuery', {
-                    //             "QueryName": "InsertAttributes",
-                    //             "Parameters": [
-                    //                 { Key: "QuoteNum", Value: quoteNum },
-                    //                 { Key: "QuoteLine", Value: dtl.QuoteLine },
-                    //                 { Key: "PartID", Value: Date().now() },
-                    //                 { Key: "AttributeID", Value: subAttr.ATTRIBUTE_ID },
-                    //                 { Key: "ParentID", Value: attr.ATTRIBUTE_ID },
-                    //                 { Key: "AttributeValue", Value: subAttrVal.ATTRIBUTE_VALUE },
-                    //                 { Key: "OtherAttributeValue", Value: 'TEST' }, //{ Key: "OtherAttributeValue", Value: (attr.ATTRIBUTE_VALUES.find(val => val.ATTRIBUTE_VALUE === 'OTHER') !== undefined) ? attr.ATTRIBUTE_VALUES.find(val => val.ATTRIBUTE_VALUE === 'OTHER').SELECTED_VALUE : '' },
-                    //                 { Key: "AttributeDesc", Value: subAttr.ATTRIBUTE_DESCRIPTION },
-                    //                 { Key: "PartNum", Value: dtl.PartNum }
-                    //             ]
-                    //         }, 0);
-                    //     });
-                    // });
-                    //}
+                    var selSubAttr = attr.ATTRIBUTE_VALUES.find(attrVal_subAttr => attrVal_subAttr.ATTRIBUTE_VALUE === attr.SELECTED_VALUE);
+                    if (selSubAttr != undefined) {
+                        console.log(selSubAttr);
+                        //if (attr.ATTRIBUTE_VALUES.find(attrVal => attrVal.ATTRIBUTE_VALUE === attr.SELECTED_VALUE).hasOwnProperty("SUB_ATTRIBUTE")) {
+                        //  console.log(attr);
+                        // attr.ATTRIBUTE_VALUES.SUB_ATTRIBUTE.map(subAttr => {
+                        //     subAttr.ATTRIBUTE_VALUES.map(subAttrVal => {
+                        //         bezl.dataService.add('QuoteAttrs_Sub', 'brdb', 'sales-rep-queries', 'ExecuteNonQuery', {
+                        //             "QueryName": "InsertAttributes",
+                        //             "Parameters": [
+                        //                 { Key: "QuoteNum", Value: quoteNum },
+                        //                 { Key: "QuoteLine", Value: dtl.QuoteLine },
+                        //                 { Key: "PartID", Value: Date().now() },
+                        //                 { Key: "AttributeID", Value: subAttr.ATTRIBUTE_ID },
+                        //                 { Key: "ParentID", Value: attr.ATTRIBUTE_ID },
+                        //                 { Key: "AttributeValue", Value: subAttrVal.ATTRIBUTE_VALUE },
+                        //                 { Key: "OtherAttributeValue", Value: 'TEST' }, //{ Key: "OtherAttributeValue", Value: (attr.ATTRIBUTE_VALUES.find(val => val.ATTRIBUTE_VALUE === 'OTHER') !== undefined) ? attr.ATTRIBUTE_VALUES.find(val => val.ATTRIBUTE_VALUE === 'OTHER').SELECTED_VALUE : '' },
+                        //                 { Key: "AttributeDesc", Value: subAttr.ATTRIBUTE_DESCRIPTION },
+                        //                 { Key: "PartNum", Value: dtl.PartNum }
+                        //             ]
+                        //         }, 0);
+                        //     });
+                        // });
+                        //}
+                    }
                 });
             }
         });
