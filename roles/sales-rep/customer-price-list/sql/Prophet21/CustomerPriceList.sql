d9dab56bf044d0d6d734d5af12e8b518d6991262
KCraneGH <KCrane@SaberLogic.com>
2017-05-26 11:10:34 -0400

    Added quantity pricing SQL.

--- roles/sales-rep/customer-price-list/sql/Prophet21/CustomerPriceList.sql
+++ roles/sales-rep/customer-price-list/sql/Prophet21/CustomerPriceList.sql
@@ -7,15 +7,40 @@ Select
 	p21_view_price_page.effective_date As StartDate,
 	p21_view_price_page.expiration_date As EndDate,
 	p21_view_price_page.price As BasePrice,
-	-- PENDING \/
-	Erp.PLPartBrk.Quantity As Quantity,
-	Erp.PLPartBrk.UnitPrice As PriceBreakUnitPrice
+	Quantity,
+	PriceBreakUnitPrice
 From 
 	p21_view_price_page with (nolock)
 	Left Outer Join p21_view_customer with(nolock) On
 	p21_view_price_page.customer_id = p21_view_customer.customer_id
 	Left Outer Join p21_view_inv_mast with(nolock) On
 	p21_view_price_page.inv_mast_uid = p21_view_inv_mast.inv_mast_uid
+	Left Outer Join (
+		Select 
+			price_page_uid,
+			Quantity,
+			PriceBreakUnitPrice
+		From p21_view_price_page with (nolock)
+		Cross Apply (
+			Select 0, calculation_value1 Union
+			Select break1, calculation_value2 Union
+			Select break2, calculation_value3 Union
+			Select break3, calculation_value4 Union
+			Select break4, calculation_value5 Union
+			Select break5, calculation_value6 Union
+			Select break6, calculation_value7 Union
+			Select break7, calculation_value8 Union
+			Select break8, calculation_value9 Union
+			Select break9, calculation_value10 Union
+			Select break10, calculation_value11 Union
+			Select break11, calculation_value12 Union
+			Select break12, calculation_value13 Union
+			Select break13, calculation_value14 Union
+			Select break14, calculation_value15
+			) ColmsToRows (Quantity, PriceBreakUnitPrice)
+		where PriceBreakUnitPrice > 0
+		) QuantityPricing On
+	p21_view_price_page.price_page_uid = QuantityPricing.price_page_uid
 Where
-	--? Erp.CustomerPriceLst.Company =  Case When 'ALL' <> 'ALL' Then 'ALL' Else p21_view_price_page.Company End And
-	p21_view_customer.customer_id = '{CustID}'
+p21_view_price_page.company_id =  CASE WHEN '{Company}' <> 'ALL' THEN '{Company}' ELSE p21_view_price_page.company_id END
+AND	p21_view_customer.customer_id_string = '{CustID}'
