e71474d74686a310e18560c80111ae933dc7143e
Bob Schnable <rschnable@saberlogic.com>
2017-06-14 12:41:59 -0400

    RS

--- roles/sales-rep/accounts-view/js/onDataChange.js
+++ roles/sales-rep/accounts-view/js/onDataChange.js
@@ -1,13 +1,15 @@
 define(["./account.js"], function (account) {
- 
-    function OnDataChange (bezl) {
+
+    function OnDataChange(bezl) {
         if (bezl.data.Accounts) {
             bezl.vars.loading = false;
+
+            console.log(bezl.data.Accounts);
 
             // If there was a previously selected account in localStorage, grab a reference
             // so we can know whether to mark them as selected
             bezl.vars.selectedAccount = {};
-            if (typeof(Storage) !== "undefined" && localStorage.getItem("selectedAccount")) {
+            if (typeof (Storage) !== "undefined" && localStorage.getItem("selectedAccount")) {
                 bezl.vars.selectedAccount = JSON.parse(localStorage.getItem("selectedAccount"));
             }
 
@@ -27,9 +29,9 @@ define(["./account.js"], function (account) {
                 // Determine the distance from the current location, if applicable
                 if (bezl.data.Accounts[i].Geocode_Location) {
                     bezl.data.Accounts[i].Distance = CalcDistance(bezl.vars.currentLat
-                                                                , bezl.vars.currentLng
-                                                                , parseFloat(bezl.data.Accounts[i].Geocode_Location.split(',')[0].split(':')[1])
-                                                                , parseFloat(bezl.data.Accounts[i].Geocode_Location.split(',')[1].split(':')[1]));
+                        , bezl.vars.currentLng
+                        , parseFloat(bezl.data.Accounts[i].Geocode_Location.split(',')[0].split(':')[1])
+                        , parseFloat(bezl.data.Accounts[i].Geocode_Location.split(',')[1].split(':')[1]));
                 }
 
                 // Set up any of the properties we wish to consolidate additional
@@ -38,7 +40,7 @@ define(["./account.js"], function (account) {
                 bezl.data.Accounts[i].CRMCalls = [];
                 bezl.data.Accounts[i].Tasks = [];
                 bezl.data.Accounts[i].Attachments = [];
-                
+
 
                 // Date pipe has rounding issue, truncate at "T" to get correct Date
                 bezl.data.Accounts[i].LastContact = (bezl.data.Accounts[i].LastContact || 'T').split("T")[0];
@@ -92,19 +94,19 @@ define(["./account.js"], function (account) {
     }
 
     function CalcDistance(lat1, lon1, lat2, lon2, unit) {
-        var radlat1 = Math.PI * lat1/180
-        var radlat2 = Math.PI * lat2/180
-        var theta = lon1-lon2
-        var radtheta = Math.PI * theta/180
+        var radlat1 = Math.PI * lat1 / 180
+        var radlat2 = Math.PI * lat2 / 180
+        var theta = lon1 - lon2
+        var radtheta = Math.PI * theta / 180
         var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
         dist = Math.acos(dist)
-        dist = dist * 180/Math.PI
+        dist = dist * 180 / Math.PI
         dist = dist * 60 * 1.1515
-        if (unit=="K") { dist = dist * 1.609344 }
-        if (unit=="N") { dist = dist * 0.8684 }
+        if (unit == "K") { dist = dist * 1.609344 }
+        if (unit == "N") { dist = dist * 0.8684 }
         return Math.round(dist)
     }
-  
+
     return {
         onDataChange: OnDataChange
     }
