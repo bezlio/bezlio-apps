5bc544099fba8244586d600fc0faeb51518816ed
Bob Schnable <rschnable@saberlogic.com>
2017-06-15 15:26:10 -0400

    RS

--- roles/sales-rep/map-view/js/customer.js
+++ roles/sales-rep/map-view/js/customer.js
@@ -1,11 +1,11 @@
 define([], function () {
- 
-    function Select (bezl, custNum) {
+
+    function Select(bezl, custNum) {
         if (bezl.vars.markers[custNum]) {
             // Locate this customer and navigate to them on the map
             bezl.vars.infoWindow.setContent(bezl.vars.mapFile.getInfoWindowContent(bezl.vars.markers[custNum].title,
-                                                                        bezl.vars.markers[custNum].data.Address,
-                                                                        bezl.vars.markers[custNum].data.Contacts));
+                bezl.vars.markers[custNum].data.Address,
+                bezl.vars.markers[custNum].data.Contacts));
 
             bezl.vars.selectedCustomer = bezl.vars.markers[custNum].data;
             var center = new bezl.vars.client.LatLng(bezl.vars.markers[custNum].lat, bezl.vars.markers[custNum].lng);
@@ -16,30 +16,37 @@ define([], function () {
             // If there is not a marker for the given address, geocode it now
             for (var i = 0; i < bezl.vars.customers.length; i++) {
                 if (bezl.vars.customers[i].custNum == custNum) {
-                    bezl.vars.mapFile.geocodeAddress(bezl, bezl.vars.customers[i]); 
+                    bezl.vars.mapFile.geocodeAddress(bezl, bezl.vars.customers[i]);
                     break;
                 }
             };
         }
     }
 
-    function RunQuery (bezl, queryName) {
+    function RunQuery(bezl, queryName) {
 
         switch (queryName) {
             case "CustList":
                 // Pull in the customer list for the logged in user
-                bezl.dataService.add('CustList','brdb','sales-rep-queries','ExecuteQuery', { 
+                bezl.dataService.add('CustList', 'brdb', 'sales-rep-queries', 'ExecuteQuery', {
                     "QueryName": "/map-view/GetCustomersWithAddress",
                     "Parameters": [
                         { "Key": "EmailAddress", "Value": bezl.env.currentUser }
-                    ] },0);
+                    ]
+                }, 0);
                 break;
-            default:
+            case "Categories":
+                bezl.dataService.add('Categories', 'brdb', 'sales-rep-queries', 'ExecuteQuery', {
+                    "QueryName": "/map-view/GetFilterCategories",
+                    "Parameters": [
+                        { "Key": "Col", "Value": bezl.vars.Column }
+                    ]
+                }, 0);
                 break;
         }
     }
 
-    function Sort (bezl, column) {
+    function Sort(bezl, column) {
         if (column == bezl.vars.sortCol) {
             // We clicked on the same column so make the sort opposite
             if (bezl.vars.sort == 'asc') {
@@ -55,50 +62,50 @@ define([], function () {
 
         if (bezl.vars.sortCol == 'Name') {
             if (bezl.vars.sort == 'asc') {
-            bezl.vars.customer = bezl.vars.customers.sort(function (a,b) {                    
-                var A = a.display.toUpperCase(); // ignore upper and lowercase
-                var B = b.display.toUpperCase(); // ignore upper and lowercase
-                if (A < B) {
-                    return -1;
-                }
-                if (A > B) {
-                    return 1;
-                }
-                // names must be equal
-                return 0;
-            });
+                bezl.vars.customer = bezl.vars.customers.sort(function (a, b) {
+                    var A = a.display.toUpperCase(); // ignore upper and lowercase
+                    var B = b.display.toUpperCase(); // ignore upper and lowercase
+                    if (A < B) {
+                        return -1;
+                    }
+                    if (A > B) {
+                        return 1;
+                    }
+                    // names must be equal
+                    return 0;
+                });
             } else {
-            bezl.vars.customer = bezl.vars.customers.sort(function (a,b) {                    
-                var A = a.display.toUpperCase(); // ignore upper and lowercase
-                var B = b.display.toUpperCase(); // ignore upper and lowercase
-                if (A > B) {
-                    return -1;
-                }
-                if (A < B) {
-                    return 1;
-                }
-                // names must be equal
-                return 0;
-            });
+                bezl.vars.customer = bezl.vars.customers.sort(function (a, b) {
+                    var A = a.display.toUpperCase(); // ignore upper and lowercase
+                    var B = b.display.toUpperCase(); // ignore upper and lowercase
+                    if (A > B) {
+                        return -1;
+                    }
+                    if (A < B) {
+                        return 1;
+                    }
+                    // names must be equal
+                    return 0;
+                });
             }
         } else {
             if (bezl.vars.sort == 'asc') {
-            bezl.vars.customer = bezl.vars.customers.sort(function (a,b) {                    
-                var A = a.distance || Number.MAX_SAFE_INTEGER;
-                var B = b.distance || Number.MAX_SAFE_INTEGER;
-                return A - B;
-            });
+                bezl.vars.customer = bezl.vars.customers.sort(function (a, b) {
+                    var A = a.distance || Number.MAX_SAFE_INTEGER;
+                    var B = b.distance || Number.MAX_SAFE_INTEGER;
+                    return A - B;
+                });
             } else {
-            bezl.vars.customer = bezl.vars.customers.sort(function (a,b) {                    
-                var A = a.distance || Number.MAX_SAFE_INTEGER;
-                var B = b.distance || Number.MAX_SAFE_INTEGER;
-                return B - A;
-            });
+                bezl.vars.customer = bezl.vars.customers.sort(function (a, b) {
+                    var A = a.distance || Number.MAX_SAFE_INTEGER;
+                    var B = b.distance || Number.MAX_SAFE_INTEGER;
+                    return B - A;
+                });
             }
         }
     }
 
-  
+
     return {
         select: Select,
         sort: Sort,
