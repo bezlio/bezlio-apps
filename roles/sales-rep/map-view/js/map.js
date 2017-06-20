711c0d673d7a04134e2d2576ff52251011e23b16
Bob Schnable <rschnable@saberlogic.com>
2017-06-20 11:46:00 -0400

    RS

--- roles/sales-rep/map-view/js/map.js
+++ roles/sales-rep/map-view/js/map.js
@@ -61,11 +61,11 @@ define([], function () {
         };
 
         bezl.vars.markers = bezl.vars.markers.filter(mark => mark !== undefined);
-        console.log(bezl.vars.markers);
 
         for (var i = 0; i < bezl.vars.customers.length; i++) {
             var custNum = bezl.vars.customers[i].key;
             if (bezl.vars.markers[custNum]) {
+                console.log(bezl.vars.customers[i]);
                 bezl.vars.customers[i].distance = Math.round(calcDistance(bezl.vars.markers[0].lat,
                     bezl.vars.markers[0].lng,
                     bezl.vars.markers[custNum].lat,
