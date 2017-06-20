cba205a142f4c50987faa0aaaec15bd4b2c2290c
Bob Schnable <rschnable@saberlogic.com>
2017-06-20 11:50:28 -0400

    RS

--- roles/sales-rep/map-view/js/customer.js
+++ roles/sales-rep/map-view/js/customer.js
@@ -165,64 +165,61 @@ define(["./map.js"], function (map) {
     function PlotData(bezl) {
         bezl.vars.markers = [];
 
-        require(['https://maps.googleapis.com/maps/api/js?key=' + bezl.vars.config.GoogleAPIKey], function () {
-            // Google Maps API and all its dependencies will be loaded here.
-            bezl.vars.client = google.maps;
-            bezl.vars.geocoder = new google.maps.Geocoder();
-            bezl.vars.directionsService = new google.maps.DirectionsService;
-            bezl.vars.directionsDisplay = new google.maps.DirectionsRenderer();
+        bezl.vars.client = google.maps;
+        bezl.vars.geocoder = new google.maps.Geocoder();
+        bezl.vars.directionsService = new google.maps.DirectionsService;
+        bezl.vars.directionsDisplay = new google.maps.DirectionsRenderer();
 
-            if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(function (position) {
-                    // First create the map
-                    //console.log($(bezl.container.nativeElement).find("div#map"));
-                    bezl.vars.map = new google.maps.Map(document.getElementById('map'), {
-                        center: { lat: position.coords.latitude, lng: position.coords.longitude },
-                        scrollwheel: false,
-                        zoom: 10
-                    });
-                    bezl.vars.directionsDisplay.setMap(bezl.vars.map);
-                    bezl.vars.directionsDisplay.setPanel(document.getElementById('directions'));
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                // First create the map
+                //console.log($(bezl.container.nativeElement).find("div#map"));
+                bezl.vars.map = new google.maps.Map(document.getElementById('map'), {
+                    center: { lat: position.coords.latitude, lng: position.coords.longitude },
+                    scrollwheel: false,
+                    zoom: 10
+                });
+                bezl.vars.directionsDisplay.setMap(bezl.vars.map);
+                bezl.vars.directionsDisplay.setPanel(document.getElementById('directions'));
 
-                    // Create an infowindow
-                    bezl.vars.infoWindow = new google.maps.InfoWindow;
+                // Create an infowindow
+                bezl.vars.infoWindow = new google.maps.InfoWindow;
 
-                    // Drop a marker for home
-                    var marker = new google.maps.Marker({
-                        position: { lat: position.coords.latitude, lng: position.coords.longitude },
-                        map: bezl.vars.map,
-                        label: 'A',
-                        title: 'You are here',
-                        data: '',
-                        lat: position.coords.latitude,
-                        lng: position.coords.longitude
-                    });
+                // Drop a marker for home
+                var marker = new google.maps.Marker({
+                    position: { lat: position.coords.latitude, lng: position.coords.longitude },
+                    map: bezl.vars.map,
+                    label: 'A',
+                    title: 'You are here',
+                    data: '',
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                });
 
-                    // Store the currently detected address
-                    bezl.vars.geocoder.geocode({ 'location': marker.position }, function (results, status) {
-                        if (status === 'OK') {
-                            bezl.vars.currentAddress = results[0].formatted_address;
-                        } else {
-                            bezl.notificationService.showError('Geocoder failed due to: ' + status);
-                        }
-                    });
+                // Store the currently detected address
+                bezl.vars.geocoder.geocode({ 'location': marker.position }, function (results, status) {
+                    if (status === 'OK') {
+                        bezl.vars.currentAddress = results[0].formatted_address;
+                    } else {
+                        bezl.notificationService.showError('Geocoder failed due to: ' + status);
+                    }
+                });
 
-                    marker.addListener('click', function () {
-                        bezl.vars.infoWindow.setContent(map.getInfoWindowContent('Current Location',
-                            bezl.vars.currentAddress,
-                            ''));
-                        bezl.vars.infoWindow.open(bezl.vars.map, marker);
-                    });
+                marker.addListener('click', function () {
+                    bezl.vars.infoWindow.setContent(map.getInfoWindowContent('Current Location',
+                        bezl.vars.currentAddress,
+                        ''));
+                    bezl.vars.infoWindow.open(bezl.vars.map, marker);
+                });
 
-                    bezl.vars.markers[0] = (marker);
-                            });
-                } else {
-                    bezl.notificationService.showError('MESSAGE: ' + "Geolocation is not supported by this browser.");
-                }
+                bezl.vars.markers[0] = (marker);
+                        });
+            } else {
+                bezl.notificationService.showError('MESSAGE: ' + "Geolocation is not supported by this browser.");
+            }
 
-                setTimeout(setFilterMap, 2500);
-            });
-
+            setTimeout(setFilterMap, 2500);
+            
         function setFilterMap() {
             bezl.vars.customers.forEach(cust => {
                 if (cust.streetAddress != null) {
