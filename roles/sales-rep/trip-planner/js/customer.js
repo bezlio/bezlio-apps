a8d0c9d0bd3837453310260c26413cc3662c9abe
Bob Schnable <rschnable@saberlogic.com>
2017-06-21 16:28:33 -0400

    RS

--- roles/sales-rep/trip-planner/js/customer.js
+++ roles/sales-rep/trip-planner/js/customer.js
@@ -109,7 +109,7 @@ define(["./map.js"], function (map) {
     function ApplyCategory(bezl, filterValue) {
         bezl.vars.selectedCustomers = [];
         bezl.vars.markers = [];
-        bezl.vars.directionsDisplay.setMap(null);
+
         bezl.vars.directionsDisplay = null;
 
         bezl.vars.custCategory = filterValue;
