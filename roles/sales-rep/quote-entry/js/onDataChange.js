0a91d6e3678a4615164d1e127cb88fdfcf7f76f5
Bob Schnable <rschnable@saberlogic.com>
2017-07-26 14:37:53 -0400

    RS

--- roles/sales-rep/quote-entry/js/onDataChange.js
+++ roles/sales-rep/quote-entry/js/onDataChange.js
@@ -25,8 +25,8 @@ define(function () {
         }
 
         var typeAheadPart = function (lineNum) {
-            $(bezl.container.nativeElement).find(".partNum").typeahead('destroy');
-            $(bezl.container.nativeElement).find(".partNum").typeahead({
+            $(bezl.container.nativeElement).find(".partNum" + lineNum).typeahead('destroy');
+            $(bezl.container.nativeElement).find(".partNum" + lineNum).typeahead({
                 order: "asc",
                 maxItem: 8,
                 display: ['PartNum'],
