774f704ee51fa5d4373141eb7f4eab54e906924c
Adam Ellis <aellis@saberlogic.com>
2017-08-09 14:13:50 -0400

    AE

--- roles/common/company-calendar/js/onDataChange.js
+++ roles/common/company-calendar/js/onDataChange.js
@@ -24,7 +24,7 @@ define(["./calendar.js"], function (calendar) {
                     center: 'title',
                     right: 'month,agendaWeek,agendaDay'
                 },
-                defaultView: 'agendaDay',
+                defaultView: bezl.vars.config.defaultView,
                 events: bezl.vars.events,
                 eventClick: function(event) {
                     if (event.url) {
