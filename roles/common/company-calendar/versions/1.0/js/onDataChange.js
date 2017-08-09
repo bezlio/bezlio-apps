2b2db7ce758b9203e3862fb6c1dbffa00d9cc7b9
Adam Ellis <aellis@saberlogic.com>
2017-08-08 22:31:23 -0400

    AE

---
+++ roles/common/company-calendar/versions/1.0/js/onDataChange.js
@@ -0,0 +1,43 @@
+define(["./calendar.js"], function (calendar) {
+ 
+    function OnDataChange (bezl) {
+        if (bezl.data.companyCalendar) {
+            bezl.vars.events = [];
+
+            bezl.data.companyCalendar.forEach(event => {
+                if (event.Active == 'X') {
+                    bezl.vars.events.push({
+                        title: event.Title,
+                        id: event.ID,
+                        start: ((!event.Start.endsWith("T00:00:00")) ? event.Start : event.Start.split('T')[0]),
+                        end: event.End,
+                        url: event.URL
+                    });
+                }
+
+            });
+
+            require(['node_modules/dist/fullcalendar.min.js'], function(calendar) {
+                $('#calendar').fullCalendar({
+                header: {
+                    left: 'prev,next today',
+                    center: 'title',
+                    right: 'month,agendaWeek,agendaDay'
+                },
+                defaultView: 'agendaDay',
+                events: bezl.vars.events,
+                eventClick: function(event) {
+                    if (event.url) {
+                        window.open(event.url);
+                        return false;
+                    }
+                }
+                });
+            });
+        }
+    }
+  
+    return {
+        onDataChange: OnDataChange
+    }
+});
