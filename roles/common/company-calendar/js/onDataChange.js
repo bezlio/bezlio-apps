3d4e74fe7d266afa1779e008096dd0811a19a6aa
Adam Ellis <aellis@saberlogic.com>
2017-08-08 22:00:30 -0400

    AE

--- roles/common/company-calendar/js/onDataChange.js
+++ roles/common/company-calendar/js/onDataChange.js
@@ -1,7 +1,40 @@
 define(["./calendar.js"], function (calendar) {
  
     function OnDataChange (bezl) {
+        if (bezl.data.companyCalendar) {
+            bezl.vars.events = [];
 
+            bezl.data.companyCalendar.forEach(event => {
+                if (event.Active == 'X') {
+                    bezl.vars.events.push({
+                        title: event.Title,
+                        id: event.ID,
+                        start: event.Start,
+                        end: event.End,
+                        url: event.URL
+                    });
+                }
+
+            });
+
+            require(['node_modules/dist/fullcalendar.min.js'], function(calendar) {
+                $('#calendar').fullCalendar({
+                header: {
+                    left: 'prev,next today',
+                    center: 'title',
+                    right: 'month,agendaWeek,agendaDay'
+                },
+                defaultView: 'agendaWeek',
+                events: bezl.vars.events,
+                eventClick: function(event) {
+                    if (event.url) {
+                        window.open(event.url);
+                        return false;
+                    }
+                }
+                });
+            });
+        }
     }
   
     return {
--- roles/common/company-calendar/js/onStartup.js
+++ roles/common/company-calendar/js/onStartup.js
@@ -2,60 +2,6 @@ define(["./calendar.js"], function (calendar) {
  
   function OnStartup (bezl) {
 
-    require(['node_modules/dist/fullcalendar.min.js'], function(calendar) {
-        $('#calendar').fullCalendar({
-          header: {
-            left: 'prev,next today',
-            center: 'title',
-            right: 'month,agendaWeek,agendaDay'
-          },
-          defaultDate: '2014-06-12',
-          defaultView: 'agendaWeek',
-          editable: true,
-          events: [
-            {
-              title: 'All Day Event',
-              start: '2014-06-01'
-            },
-            {
-              title: 'Long Event',
-              start: '2014-06-07',
-              end: '2014-06-10'
-            },
-            {
-              id: 999,
-              title: 'Repeating Event',
-              start: '2014-06-09T16:00:00'
-            },
-            {
-              id: 999,
-              title: 'Repeating Event',
-              start: '2014-06-16T16:00:00'
-            },
-            {
-              title: 'Meeting',
-              start: '2014-06-12T10:30:00',
-              end: '2014-06-12T12:30:00'
-            },
-            {
-              title: 'Lunch',
-              start: '2014-06-12T12:00:00'
-            },
-            {
-              title: 'Birthday Party',
-              start: '2014-06-13T07:00:00'
-            },
-            {
-              title: 'Click for Google',
-              url: 'http://google.com/',
-              start: '2014-06-28'
-            }
-          ]
-        });
-        
-      
-    });
-
     // Load up the calendar details from the data source
     bezl.dataService.add('companyCalendar',
                          'brdb',
