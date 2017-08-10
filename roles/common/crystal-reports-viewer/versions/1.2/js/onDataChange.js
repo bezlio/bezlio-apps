3ec0367828a6edaea3c181dcf2c2ada04e735d6e
Adam Ellis <aellis@saberlogic.com>
2017-08-10 10:45:57 -0400

    AE

--- roles/common/crystal-reports-viewer/versions/1.2/js/onDataChange.js
+++ roles/common/crystal-reports-viewer/versions/1.2/js/onDataChange.js
@@ -12,8 +12,6 @@ define(["./report.js"], function (report) {
               paths: {'pdfjs-dist': 'https://npmcdn.com/pdfjs-dist'}
             });
             require(['pdfjs-dist/build/pdf'], function (PDFJS) {
-                alert(PDFJS);
-                alert(window.PDFJS);
 
                 // Using DocumentInitParameters object to load binary data.
                 var loadingTask = PDFJS.getDocument({data: pdfData});
@@ -49,30 +47,6 @@ define(["./report.js"], function (report) {
                     console.error(reason);
                 });
             });
-
-            /*var bytesLength = byteCharacters.length;
-            var slicesCount = Math.ceil(bytesLength / sliceSize);
-            var byteArrays = new Array(slicesCount);
-            for (var sliceIndex = 0; sliceIndex < slicesCount; ++sliceIndex) {
-                var begin = sliceIndex * sliceSize;
-                var end = Math.min(begin + sliceSize, bytesLength);
-                var bytes = new Array(end - begin);
-                for (var offset = begin, i = 0 ; offset < end; ++i, ++offset) {
-                    bytes[i] = byteCharacters[offset].charCodeAt(0);
-                }
-                byteArrays[sliceIndex] = new Uint8Array(bytes);
-            }
-
-            var file = new Blob(byteArrays, {type: 'application/pdf'});
-
-            if (screen.width <= 480) {
-                report.back(bezl);
-                saveAs(file);
-            } else {
-                var fileURL = URL.createObjectURL(file);
-                var viewer = $(bezl.container.nativeElement).find('#viewer')[0];
-                viewer.src = fileURL;
-            }*/
 
             bezl.vars.reportLoading = false;
 
