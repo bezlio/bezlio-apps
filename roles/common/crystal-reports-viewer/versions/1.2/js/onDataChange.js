01947017b37aac5fd69634a31ab1807c2ee9ef77
Adam Ellis <aellis@saberlogic.com>
2017-08-10 09:04:51 -0400

    AE

--- roles/common/crystal-reports-viewer/versions/1.2/js/onDataChange.js
+++ roles/common/crystal-reports-viewer/versions/1.2/js/onDataChange.js
@@ -8,50 +8,49 @@ define(["./report.js"], function (report) {
         if (bezl.data.Report) { 
             var pdfData = atob(bezl.data.Report);
 
-            require(['https://mozilla.github.io/pdf.js/build/pdf.js'], function(functions) {
+            require.config({paths: {'//mozilla.github.io/pdf.js/build/pdf': 'pdf'}});
+            require(['pdfjs-dist/build/pdf'], function(pdf) {
 
-                // Disable workers to avoid yet another cross-origin issue (workers need
-                // the URL of the script to be loaded, and dynamically loading a cross-origin
-                // script does not work).
-                // PDFJS.disableWorker = true;
-
-                // The workerSrc property shall be specified.
-                PDFJS.workerSrc = '//mozilla.github.io/pdf.js/build/pdf.worker.js';
+                pdf.PDFJS.disableWorker = true;
+                console.log('PDFJS Module: ' + pdf + '<br>');
+                console.log('PDFJS Global: ' + window.PDFJS);
 
                 // Using DocumentInitParameters object to load binary data.
-                var loadingTask = PDFJS.getDocument({data: pdfData});
+                var loadingTask = pdf.PDFJS.getDocument({data: pdfData});
                 loadingTask.promise.then(function(pdf) {
-                console.log('PDF loaded');
-                
-                // Fetch the first page
-                var pageNumber = 1;
-                pdf.getPage(pageNumber).then(function(page) {
-                    console.log('Page loaded');
+                    console.log('PDF loaded');
                     
-                    var scale = 1.5;
-                    var viewport = page.getViewport(scale);
+                    // Fetch the first page
+                    var pageNumber = 1;
+                    pdf.getPage(pageNumber).then(function(page) {
+                        console.log('Page loaded');
+                        
+                        var scale = 1.5;
+                        var viewport = page.getViewport(scale);
 
-                    // Prepare canvas using PDF page dimensions
-                    var canvas = document.getElementById('viewer');
-                    var context = canvas.getContext('2d');
-                    canvas.height = viewport.height;
-                    canvas.width = viewport.width;
+                        // Prepare canvas using PDF page dimensions
+                        var canvas = document.getElementById('viewer');
+                        var context = canvas.getContext('2d');
+                        canvas.height = viewport.height;
+                        canvas.width = viewport.width;
 
-                    // Render PDF page into canvas context
-                    var renderContext = {
-                    canvasContext: context,
-                    viewport: viewport
-                    };
-                    var renderTask = page.render(renderContext);
-                    renderTask.then(function () {
-                    console.log('Page rendered');
+                        // Render PDF page into canvas context
+                        var renderContext = {
+                        canvasContext: context,
+                        viewport: viewport
+                        };
+                        var renderTask = page.render(renderContext);
+                        renderTask.then(function () {
+                        console.log('Page rendered');
+                        });
                     });
-                });
                 }, function (reason) {
-                // PDF loading error
-                console.error(reason);
+                    // PDF loading error
+                    console.error(reason);
                 });
+                
             });
+
             /*var bytesLength = byteCharacters.length;
             var slicesCount = Math.ceil(bytesLength / sliceSize);
             var byteArrays = new Array(slicesCount);
