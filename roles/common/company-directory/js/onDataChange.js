e77d3bd935f134ff83cda80facff8676e6f482de
Adam Ellis <aellis@saberlogic.com>
2017-08-10 08:47:41 -0400

    Attempt with pdf.js

---
+++ roles/common/company-directory/js/onDataChange.js
@@ -0,0 +1,10 @@
+define(["./directory.js"], function (directory) {
+ 
+    function OnDataChange (bezl) {
+
+    }
+  
+    return {
+        onDataChange: OnDataChange
+    }
+});
--- roles/common/crystal-reports-viewer/versions/1.2/js/onDataChange.js
+++ roles/common/crystal-reports-viewer/versions/1.2/js/onDataChange.js
@@ -6,9 +6,53 @@ define(["./report.js"], function (report) {
         }
 
         if (bezl.data.Report) { 
-            var sliceSize = 1024;
-            var byteCharacters = atob(bezl.data.Report);
-            var bytesLength = byteCharacters.length;
+            var pdfData = atob(bezl.data.Report);
+
+            require(['https://mozilla.github.io/pdf.js/build/pdf.js'], function(functions) {
+
+                // Disable workers to avoid yet another cross-origin issue (workers need
+                // the URL of the script to be loaded, and dynamically loading a cross-origin
+                // script does not work).
+                // PDFJS.disableWorker = true;
+
+                // The workerSrc property shall be specified.
+                PDFJS.workerSrc = '//mozilla.github.io/pdf.js/build/pdf.worker.js';
+
+                // Using DocumentInitParameters object to load binary data.
+                var loadingTask = PDFJS.getDocument({data: pdfData});
+                loadingTask.promise.then(function(pdf) {
+                console.log('PDF loaded');
+                
+                // Fetch the first page
+                var pageNumber = 1;
+                pdf.getPage(pageNumber).then(function(page) {
+                    console.log('Page loaded');
+                    
+                    var scale = 1.5;
+                    var viewport = page.getViewport(scale);
+
+                    // Prepare canvas using PDF page dimensions
+                    var canvas = document.getElementById('viewer');
+                    var context = canvas.getContext('2d');
+                    canvas.height = viewport.height;
+                    canvas.width = viewport.width;
+
+                    // Render PDF page into canvas context
+                    var renderContext = {
+                    canvasContext: context,
+                    viewport: viewport
+                    };
+                    var renderTask = page.render(renderContext);
+                    renderTask.then(function () {
+                    console.log('Page rendered');
+                    });
+                });
+                }, function (reason) {
+                // PDF loading error
+                console.error(reason);
+                });
+            });
+            /*var bytesLength = byteCharacters.length;
             var slicesCount = Math.ceil(bytesLength / sliceSize);
             var byteArrays = new Array(slicesCount);
             for (var sliceIndex = 0; sliceIndex < slicesCount; ++sliceIndex) {
@@ -30,7 +74,7 @@ define(["./report.js"], function (report) {
                 var fileURL = URL.createObjectURL(file);
                 var viewer = $(bezl.container.nativeElement).find('#viewer')[0];
                 viewer.src = fileURL;
-            }
+            }*/
 
             bezl.vars.reportLoading = false;
 
