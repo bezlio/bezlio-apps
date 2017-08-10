7990cef3885578da981786c916551883b8a55b38
Adam Ellis <aellis@saberlogic.com>
2017-08-10 16:13:13 -0400

    Add common / company calendar #350 (#351)
    Initial 1.0 version of the Company Calendar app.

---
+++ roles/common/company-calendar/versions/1.0/js/onDataChange.js
@@ -0,0 +1,29 @@
+define(["./calendar.js"], function (calendar) {
+ 
+    function OnDataChange (bezl) {
+        if (bezl.data.companyCalendar) {
+            bezl.vars.config.calendarOptions.events = [];
+
+            bezl.data.companyCalendar.forEach(event => {
+                if (event.Active == 'X') {
+                    bezl.vars.config.calendarOptions.events.push({
+                        title: event.Title,
+                        id: event.ID,
+                        start: ((!event.Start.endsWith("T00:00:00")) ? event.Start : event.Start.split('T')[0]),
+                        end: event.End,
+                        url: event.URL
+                    });
+                }
+
+            });
+
+            require(['node_modules/dist/fullcalendar.min.js'], function(calendar) {
+                $('#calendar').fullCalendar(bezl.vars.config.calendarOptions);
+            });
+        }
+    }
+  
+    return {
+        onDataChange: OnDataChange
+    }
+});
