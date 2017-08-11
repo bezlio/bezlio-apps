d51718456740a40f2692af4b72181cce1262b62e
Adam Ellis <aellis@saberlogic.com>
2017-08-11 07:13:18 -0400

    AE

--- roles/sales-rep/accounts-view/versions/1.2/js/onDataChange.js
+++ roles/sales-rep/accounts-view/versions/1.2/js/onDataChange.js
@@ -59,7 +59,7 @@ define(["./account.js"], function (account) {
 
                     if (bezl.data[n.id]) {
                         platform.onAddNoteResponse(bezl, n);
-                    } else if (!n.processed && now - n.lastAttempt > bezl.vars.config.retryInterval && n.retryCount <= bezl.vars.config.maxRetryCount) {
+                    } else if (!n.processed && (now - n.lastAttempt > bezl.vars.config.retryInterval / 1000) && n.retryCount <= bezl.vars.config.maxRetryCount) {
                         platform.submitNote(bezl, n);
                     }
 
