19f53fdaff0eeb562e1e10a6c430dcd80adff79d
Bob Schnable <rschnable@saberlogic.com>
2017-08-17 15:31:56 -0400

    RS

--- roles/common/ssrs-report-viewer/js/onDataChange.js
+++ roles/common/ssrs-report-viewer/js/onDataChange.js
@@ -15,26 +15,28 @@ define(function () {
             var byteCharacters = atob(bezl.data.Report);
             console.log(byteCharacters);
 
-            var bytesLength = byteCharacters.length;
-            var sliceCount = Math.ceil(bytesLength / sliceSize);
-            var byteArrays = new Array(sliceCount);
+            $(bezl.container.nativeElement).find('#viewer').html(byteCharacters);
 
-            for (var sliceIndex = 0; sliceIndex < sliceCount; sliceIndex++) {
-                var begin = sliceIndex * sliceSize;
-                var end = Math.min(begin + sliceSize, bytesLength);
-                var bytes = new Array(end - begin);
+            // var bytesLength = byteCharacters.length;
+            // var sliceCount = Math.ceil(bytesLength / sliceSize);
+            // var byteArrays = new Array(sliceCount);
 
-                for (var offset = begin, i = 0; offset < end; i++ , ++offset) {
-                    bytes[i] = byteCharacters[offset].charCodeAt(0);
-                }
-                byteArrays[sliceIndex] = new Uint8Array(bytes);
-            }
+            // for (var sliceIndex = 0; sliceIndex < sliceCount; sliceIndex++) {
+            //     var begin = sliceIndex * sliceSize;
+            //     var end = Math.min(begin + sliceSize, bytesLength);
+            //     var bytes = new Array(end - begin);
 
-            var file = new Blob(byteArrays, { type: 'application/pdf' });
-            var fileURL = URL.createObjectURL(file);
-            var viewer = $(bezl.container.nativeElement).find('#viewer')[0];
-            viewer.src = fileURL;
-            bezl.vars.reportLoading = false;
+            //     for (var offset = begin, i = 0; offset < end; i++ , ++offset) {
+            //         bytes[i] = byteCharacters[offset].charCodeAt(0);
+            //     }
+            //     byteArrays[sliceIndex] = new Uint8Array(bytes);
+            // }
+
+            // var file = new Blob(byteArrays, { type: 'application/pdf' });
+            // var fileURL = URL.createObjectURL(file);
+            // var viewer = $(bezl.container.nativeElement).find('#viewer')[0];
+            // viewer.src = fileURL;
+            // bezl.vars.reportLoading = false;
         }
     }
 
