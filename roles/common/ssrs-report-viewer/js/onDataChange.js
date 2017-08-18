12f80ee6350426320bf04220491b190d66e52822
Bob Schnable <rschnable@saberlogic.com>
2017-08-18 11:54:50 -0400

    RS

--- roles/common/ssrs-report-viewer/js/onDataChange.js
+++ roles/common/ssrs-report-viewer/js/onDataChange.js
@@ -18,12 +18,11 @@ define(function () {
             //$(bezl.container.nativeElement).find('#viewer')[0].innerHtml = byteCharacters;
 
             var iFrame = $(bezl.container.nativeElement).find('#viewer')[0];
-            iFrame.data = byteCharacters;
-            // iFrame = iFrame.contentWindow || (iFrame.contentDocument.document || iFrame.contentDocument);
+            iFrame = iFrame.contentWindow || (iFrame.contentDocument.document || iFrame.contentDocument);
 
-            // iFrame.document.open();
-            // iFrame.document.write(byteCharacters);
-            // iFrame.close();
+            iFrame.document.open();
+            iFrame.document.write(byteCharacters);
+            iFrame.close();
 
             // var iframe = document.getElementById('iframeID');
             // iframe = iframe.contentWindow || ( iframe.contentDocument.document || iframe.contentDocument);
