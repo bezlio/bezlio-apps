81a9eee3e65b27fbf6aa6a7655edeca23e77ee96
Adam Ellis <aellis@saberlogic.com>
2017-08-22 15:18:38 -0400

    AE

--- roles/ceo/comparative-sales-breakdown/js/onDataChange.js
+++ roles/ceo/comparative-sales-breakdown/js/onDataChange.js
@@ -4,11 +4,18 @@ define(["./app.js"], function (app) {
         if (bezl.data.SummaryData) {
             bezl.vars.loadingSummaryData = false;
             // This variable is used to prorate previous YTD numbers on the details window
-            bezl.vars.currYearPercent = bezl.data.SummaryData[0]["Portion Of Year"];
+            bezl.vars.currYearPercent = bezl.data.SummaryData[0]['Portion Of Year'];
         }
 
         if (bezl.data.ByProduct) {
             bezl.vars.loadingByProduct = false;
+
+            // Fix up the column headers since they cannot come through on the pivor table
+            bezl.data.ByProduct.forEach(p => {
+                p.Product = p.Column1 || p['Column 1'];
+                p.YTDSales = p.Column3 || p['Column 3'];
+                p.PriorSales = p.Column2 || p['Column 2'];
+            });
         }
 
         if (bezl.data.ByMonth) {
