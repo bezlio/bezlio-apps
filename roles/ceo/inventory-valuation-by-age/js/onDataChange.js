2095ae32c30f0225d98439fc8c5e50c5afdfaaf9
Adam Ellis <aellis@saberlogic.com>
2017-08-22 21:00:23 -0400

    AE

--- roles/ceo/inventory-valuation-by-age/js/onDataChange.js
+++ roles/ceo/inventory-valuation-by-age/js/onDataChange.js
@@ -68,7 +68,7 @@ define(["./app.js"], function (app) {
                 {
                 label: 'Total',
                 fillcolor: "8c8c8c",
-                value: totalCost.toString().replace(/B(?=(d{3})+(?!d))/g, ","),
+                value: Math.round(totalCost).toString().replace(/B(?=(d{3})+(?!d))/g, ","),
                 data: []
                 }
             ]
@@ -78,7 +78,7 @@ define(["./app.js"], function (app) {
             locations.forEach(l => {
             bezl.vars.dataSource.data[0].data.push({
                 label: l.name,
-                value: l.totalCost.toString().replace(/B(?=(d{3})+(?!d))/g, ","),
+                value: Math.round(l.totalCost).toString().replace(/B(?=(d{3})+(?!d))/g, ","),
                 data: l.data
             });
             }); 
