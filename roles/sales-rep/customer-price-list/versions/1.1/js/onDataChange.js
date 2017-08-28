1e2a6be1196eca9fef3cbb0928727ab8acbe04c7
Seth Remington <sremington@saberlogic.com>
2017-08-28 14:01:42 -0400

    - Added version 1.1 of Customer Price List app - Added version 1.1 of Invoice Inquiry app - Added version 1.1 of Order Inquiry app - Added version 1.2 of RMA Inquiry app

---
+++ roles/sales-rep/customer-price-list/versions/1.1/js/onDataChange.js
@@ -0,0 +1,76 @@
+define(function () {
+ 
+    function OnDataChange (bezl) {
+        if (bezl.data.PriceList) {
+            
+            bezl.vars.PriceList = new Array();
+
+            // If there was a previously selected PArt in localStorage, grab a reference
+            // so we can know whether to mark them as selected
+            bezl.vars.selectedPart = {};
+            if (typeof(Storage) !== "undefined" && localStorage.getItem("selectedPart")) {
+                bezl.vars.selectedPart = JSON.parse(localStorage.getItem("selectedPart"));
+            }
+
+            var tempPart = {};
+            var tempLine = {};
+            
+            // Organize new object
+            for(var i = 0; i < bezl.data.PriceList.length; i++) {
+
+                 //clear temps
+                tempPart = {};
+                tempLine = {};
+
+                // If invoice num already exist in new object, move invoice lines over
+                if (bezl.vars.PriceList.find(invoice => invoice.PartNum == bezl.data.PriceList[i].PartNum)) {
+                    // Line
+                    tempLine.PriceBreakUnitPrice = bezl.data.PriceList[i].PriceBreakUnitPrice;
+                    tempLine.Quantity = bezl.data.PriceList[i].Quantity;
+                    // Push line into invoice
+                    bezl.vars.PriceList[bezl.vars.PriceList.findIndex(inv => inv.PartNum == bezl.data.PriceList[i].PartNum)].PartLines.push(tempLine);
+                } else {
+
+                    // Part
+                    tempPart.PartNum = bezl.data.PriceList[i].PartNum || "";
+                    tempPart.PartDescription = bezl.data.PriceList[i].PartDescription || "";
+                    tempPart.StartDate = bezl.data.PriceList[i].StartDate || "";
+                    tempPart.EndDate = bezl.data.PriceList[i].EndDate || "";
+                    tempPart.BasePrice = bezl.data.PriceList[i].BasePrice;
+
+                    // Add a Selected property to the account record
+                    if (bezl.data.PriceList[i].PartNum == bezl.vars.selectedPart.PartNum) {
+                        tempPart.Selected = true;
+                    } else {
+                        tempPart.Selected = false;
+                    }
+
+                    // Line
+                    tempLine.PriceBreakUnitPrice = bezl.data.PriceList[i].PriceBreakUnitPrice;
+                    tempLine.Quantity = bezl.data.PriceList[i].Quantity;
+                    // Push line into invoice
+                    tempPart.PartLines = new Array();
+                    tempPart.PartLines.push(tempLine); 
+
+                    // Push invoice into final data var
+                    bezl.vars.PriceList.push(tempPart); 
+                }
+            }
+            bezl.vars.loading = false;
+        }
+
+        if(bezl.data.Accounts) {
+            bezl.vars.custList = bezl.data.Accounts;
+            if(!bezl.vars.custList.find(a => a.ID == "ALL_ACCOUNTS")) {
+                bezl.vars.custList.unshift({ID: "ALL_ACCOUNTS", Name: "All Accounts"});
+            }
+            bezl.vars.loading = false;
+            bezl.dataService.Accounts = null;
+            bezl.dataService.remove('Accounts');
+        }
+    }
+
+    return {
+            onDataChange: OnDataChange
+        }
+});
--- roles/sales-rep/rma-inquiry/versions/CHANGELOG.md
+++ roles/sales-rep/rma-inquiry/versions/CHANGELOG.md
@@ -5,6 +5,11 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 
 ## [Unreleased]
 
+## [1.2] - 2017-08-28
+### Added
+- New setConfig parameter "autoLoad": With the loading of the page, if true, the bezl will run the query for the selected customer. If false, the bezl will require a manual search to run the query. Default is false.
+- New setConfig parameter "autoLoadOnSelection": With the selection of a customer (with accounts view on the same panel), if true, the bezl will run the query for the selected customer. If false, the bezl will require a manual search to run the query. Default is true.
+
 ## [1.1] - 2017-08-24
 ### Fixed
 - Run correct query when "All Accounts" selected and the "Search" button is clicked.
