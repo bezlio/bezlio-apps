30584a65f377e43de082ee3ecfd48634a24056b6
Bob Schnable <rschnable@saberlogic.com>
2017-11-01 11:05:24 -0400

    RS

--- roles/sales-rep/tasks-view/versions/1.1/js/task.js
+++ roles/sales-rep/tasks-view/versions/1.1/js/task.js
@@ -1,26 +1,28 @@
 define(function () {
- 
-    function RunQuery (bezl, queryName) {
+
+    function RunQuery(bezl, queryName) {
 
         switch (queryName) {
             case "TaskTypes":
                 // Pull in the task types
-                bezl.dataService.add('TaskTypes','brdb','sales-rep-queries','ExecuteQuery', { 
+                bezl.dataService.add('TaskTypes', 'brdb', 'sales-rep-queries', 'ExecuteQuery', {
                     "QueryName": "GetTaskTypes",
                     "Parameters": [
                         { "Key": "EmailAddress", "Value": bezl.env.currentUser }
-                    ] },0);
+                    ]
+                }, 0);
                 break;
             case "Tasks":
-                bezl.vars.loading = true; 
+                bezl.vars.loading = true;
 
                 // Pull in the accounts list for the logged in user
-                bezl.dataService.add('Tasks','brdb','sales-rep-queries','ExecuteQuery', { 
+                bezl.dataService.add('Tasks', 'brdb', 'sales-rep-queries', 'ExecuteQuery', {
                     "QueryName": "GetAccountTasks",
                     "Parameters": [
                         { "Key": "EmailAddress", "Value": bezl.env.currentUser },
                         { "Key": "ID", "Value": bezl.vars.selectedAccount.ID }
-                    ] },0);
+                    ]
+                }, 0);
                 break;
             default:
                 break;
@@ -31,35 +33,39 @@ define(function () {
         task.Selected = !task.Selected;
     }
 
-    function AddTask (bezl) {
+    function AddTask(bezl) {
         // Since alternate systems may require different columns for newly added tasks, direct to
         // the libraries folder
         if (bezl.vars.Platform == "Epicor10" || bezl.vars.Platform == "Epicor905") {
-            require([bezl.vars.config.baseLibraryUrl + 'epicor/crm.js'], function(functions) {
+            require([bezl.vars.config.baseLibraryUrl + 'epicor/crm.js'], function (functions) {
                 var taskType = '';
                 if (bezl.data.TaskTypes) {
                     taskType = bezl.data.TaskTypes[0].TaskType;
                 }
 
+                if (!bezl.vars.selectedAccount.Tasks) {
+                    bezl.vars.selectedAccount.Tasks = [];
+                }
+
                 bezl.vars.selectedAccount.Tasks.push(
                     functions.getNewTask(bezl.vars.selectedAccount.CustNum,
-                                        bezl.vars.selectedAccount.SalesRep,
-                                        taskType)
+                        bezl.vars.selectedAccount.SalesRep,
+                        taskType)
                 );
 
                 // Order the added tasks to the top
-                bezl.vars.selectedAccount.Tasks.sort(function(a, b) {
+                bezl.vars.selectedAccount.Tasks.sort(function (a, b) {
                     if (a.RowState && a.RowState == 'Added') {
                         return -1;
                     } else {
                         return 1;
                     }
                 });
-            }); 
+            });
         }
     }
 
-    function CancelAddTask (bezl, task) {
+    function CancelAddTask(bezl, task) {
         for (var i = 0; i < bezl.vars.selectedAccount.Tasks.length; i++) {
             if (bezl.vars.selectedAccount.Tasks[i] == task) {
                 bezl.vars.selectedAccount.Tasks.splice(i, 1);
@@ -70,17 +76,17 @@ define(function () {
         }
     }
 
-    function UpdateTasks (bezl) {
+    function UpdateTasks(bezl) {
         // Since alternate systems may require different columns for newly added tasks, direct to
         // the libraries folder
         if (bezl.vars.Platform == "Epicor10" || bezl.vars.Platform == "Epicor905") {
-            require([bezl.vars.config.baseLibraryUrl + 'epicor/crm.js'], function(functions) {
+            require([bezl.vars.config.baseLibraryUrl + 'epicor/crm.js'], function (functions) {
                 functions.updateTasks(bezl,
-                                      bezl.vars.Platform,
-                                      bezl.vars.selectedAccount.Tasks);
+                    bezl.vars.Platform,
+                    bezl.vars.selectedAccount.Tasks);
 
                 bezl.notificationService.showSuccess('Task changes are being saved.  You will be notified if any errors occurs.');
-            }); 
+            });
         }
 
         localStorage.setItem('selectedAccount', JSON.stringify(bezl.vars.selectedAccount));
