7cec82291fdcf937d9722c437ba2fff0af54feed
Bob Schnable <rschnable@saberlogic.com>
2017-11-06 14:56:56 -0500

    RS

--- roles/sales-rep/tasks-view/versions/1.1/js/onStartup.js
+++ roles/sales-rep/tasks-view/versions/1.1/js/onStartup.js
@@ -1,23 +1,23 @@
 define(["./task.js"], function (task) {
- 
-  function OnStartup (bezl) {        
-      // Load the object for the selected customer from local storage into
-      // a variable we can work with
-      if (typeof(Storage) !== "undefined" && localStorage.getItem("selectedAccount")) {
-          bezl.vars.selectedAccount = JSON.parse(localStorage.getItem("selectedAccount"));
-      }
 
-      // Also pull in the list of defined task types.  This is expecting a plugin instance
-      // to be defined in BRDB named sales-rep-queries which points to a data source for call
-      // types
-      bezl.dataService.add('TaskTypes','brdb','sales-rep-queries','ExecuteQuery', { "QueryName": "GetTaskTypes" },0);
+  function OnStartup(bezl) {
+    // Load the object for the selected customer from local storage into
+    // a variable we can work with
+    if (typeof (Storage) !== "undefined" && localStorage.getItem("selectedAccount")) {
+      bezl.vars.selectedAccount = JSON.parse(localStorage.getItem("selectedAccount"));
+    }
 
-      $("#bezlpanel").on("selectAccount", function(event, param1) {
-        bezl.vars.selectedAccount = param1;
-      });
+    // Also pull in the list of defined task types.  This is expecting a plugin instance
+    // to be defined in BRDB named sales-rep-queries which points to a data source for call
+    // types
+    bezl.dataService.add('TaskTypes', 'brdb', 'sales-rep-queries', 'ExecuteQuery', { "QueryName": "GetTaskTypes" }, 0);
+
+    $("#bezlpanel").on("selectAccount", function (event, param1) {
+      bezl.vars.selectedAccount = param1;
+    });
 
   }
-  
+
   return {
     onStartup: OnStartup
   }
--- roles/sales-rep/tasks-view/versions/1.2/js/onDataChange.js
+++ roles/sales-rep/tasks-view/versions/1.2/js/onDataChange.js
@@ -15,15 +15,31 @@ define(function () {
             if (bezl.data.UpdateTasks.BOUpdError && bezl.data.UpdateTasks.BOUpdError.length > 0) {
                 bezl.notificationService.showCriticalError(JSON.stringify(bezl.data.UpdateTasks.BOUpdError));
             } else {
+                // changed to be updated so all new tasks will be ready for individual saving
                 for (var i = 0; i < bezl.vars.selectedAccount.Tasks.length; i++) {
-                    bezl.vars.selectedAccount.Tasks[i].RowState = '';
+                    bezl.vars.selectedAccount.Tasks[i].RowState = 'Updated';
                 }
                 // Trigger the "updateTask" event for any bezls that need to know about the new/updated task
                 $('#bezlpanel').trigger('updateTask', [bezl.vars.selectedAccount]);
+
+                localStorage.setItem('selectedAccount', JSON.stringify(bezl.vars.selectedAccount));
             }
 
             bezl.data.UpdateTasks = null;
             bezl.dataService.remove('UpdateTasks');
+        }
+
+        //added ability to refresh tasks from Epicor side
+        if (bezl.data.Tasks) {
+            bezl.vars.selectedAccount.Tasks = [];
+            bezl.data.Tasks.forEach(task => {
+                task.RowState = "Updated";
+                bezl.vars.selectedAccount.Tasks.push(task);
+            });
+
+            localStorage.setItem('selectedAccount', JSON.stringify(bezl.vars.selectedAccount));
+
+            bezl.dataService.remove('Tasks');
         }
     }
 
