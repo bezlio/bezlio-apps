a651cb721ee4194c2dffcc7d903e11b570d94623
Bob Schnable <rschnable@saberlogic.com>
2017-11-15 14:45:54 -0500

    RS

--- roles/sales-rep/quote-entry/js/quote.js
+++ roles/sales-rep/quote-entry/js/quote.js
@@ -311,8 +311,7 @@ define(['../../../../libraries/epicor/quote.js'], function (quote_lib) {
                 this.runQuery(bezl, "Attributes");
             }
         } else {
-            console.log(line);
-            //quote_lib.saveAttributes(bezl, bezl.vars.Connection, bezl.vars.Company, line.quoteNum, line);
+            quote_lib.saveAttributes(bezl, bezl.vars.Connection, bezl.vars.Company, line.QuoteNum, line);
         }
     }
 
