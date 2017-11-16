31defb5580e1e7a8e9dc523574cad542bbd141c8
Bob Schnable <rschnable@saberlogic.com>
2017-11-16 13:27:52 -0500

    RS

--- roles/sales-rep/quote-entry/js/quote.js
+++ roles/sales-rep/quote-entry/js/quote.js
@@ -344,8 +344,7 @@ define(['../../../../libraries/epicor/quote.js'], function (quote_lib) {
         // console.log("SelectedAttribute: " + selectedAttribute);
         // console.log("Attr Value: " + attributeValue);
         var x = bezl.data.QuoteDtls.find(dtl => dtl.QuoteLine == lineNum)
-            .Attributes.find(attr => attr.ATTRIBUTE_ID === attributeID)
-            .ATTRIBUTE_VALUES.find(attrVal => attrVal.ATTRIBUTE_VALUE_LABEL === selectedAttribute);
+            .Attributes.find(attr => attr.ATTRIBUTE_ID === attributeID);
 
         console.log(bezl.data.QuoteDtls);
         console.log(x);
