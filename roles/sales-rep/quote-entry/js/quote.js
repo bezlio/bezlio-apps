dce02b1276262ee6a6eca89a4f2a5a066c970c29
Bob Schnable <rschnable@saberlogic.com>
2017-12-04 09:56:33 -0500

    RS

--- roles/sales-rep/quote-entry/js/quote.js
+++ roles/sales-rep/quote-entry/js/quote.js
@@ -330,16 +330,16 @@ define(['../../../../libraries/epicor/quote.js'], function (quote_lib) {
         //driving/dependent attribute functionality
         if (bezl.vars.attributes.find(attr => attr.ATTRIBUTE_ID === attributeID).hasOwnProperty('DRIVING_ATTRIBUTE')) {
             bezl.data.QuoteDtls.find(dtl => dtl.QuoteLine === lineNum).Attributes.map(attr => {
-                console.log("Quote Attributes: " + attr);
-                console.log(attr);
-                attr.ATTRIBUTE_VALUES.map(attrVal => {
-                    attrVal.Display = true;
-                    console.log(attrVal);
-                    if (attrVal.hasOwnProperty('DEPENDENT_ATTRIBUTE')) {
-                        if (attrVal.DEPENDENT_ATTRIBUTE.filter(depAttr => depAttr.ATTRIBUTE_ID === attributeID && depAttr.ATTRIBUTE_VALUE === attributeValue).length === 0)
-                            attrVal.Display = false;
-                    }
-                });
+                if (!attr.hasOwnProperty('SELECTED_VALUE')) {
+                    attr.ATTRIBUTE_VALUES.map(attrVal => {
+                        attrVal.Display = true;
+                        console.log(attrVal);
+                        if (attrVal.hasOwnProperty('DEPENDENT_ATTRIBUTE')) {
+                            if (attrVal.DEPENDENT_ATTRIBUTE.filter(depAttr => depAttr.ATTRIBUTE_ID === attributeID && depAttr.ATTRIBUTE_VALUE === attributeValue).length === 0)
+                                attrVal.Display = false;
+                        }
+                    });
+                }
             });
         }
     }
