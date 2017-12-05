27ee61b126be3eb4cee649a7bc0ae25cf51b0887
Bob Schnable <rschnable@saberlogic.com>
2017-12-05 11:35:58 -0500

    RS

--- libraries/epicor/quote.js
+++ libraries/epicor/quote.js
@@ -220,6 +220,7 @@ define(function () {
     function SaveAttributes(bezl, connection, company, quoteNum, dtl) {
         var attributeConcat = dtl.PartNum + ": "; //line description for configured lines
 
+        console.log(dtl.Attributes);
         if (dtl.Attributes.find(attrID => attrID.ATTRIBUTE_ID === "000_QUANTITY").ATTRIBUTE_VALUES.length > 0) {
             this.saveQuote(bezl, connection, company, bezl.vars.MktgEvntSeq, bezl.vars.quoteData);
         }
