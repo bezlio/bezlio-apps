432dd3fd63142fd9cb79161970db580c7f0526b6
Bob Schnable <rschnable@saberlogic.com>
2017-12-06 14:35:07 -0500

    RS

--- roles/sales-rep/quote-entry/js/onDataChange.js
+++ roles/sales-rep/quote-entry/js/onDataChange.js
@@ -126,11 +126,11 @@ define(function () {
                                 // console.log(attrFnd);
                                 subAttrValList.forEach(subAttrVal => {
                                     // console.log(subAttrVal);
-                                    var attrFnd_subAttr = attrFnd.ATTRIBUTE_VALUES.find(attrFndVal => attrFndVal.ATTRIBUTE_VALUE_LABEL === subAttrVal.Character03);
-                                    console.log(subAttrVal.Key4.substring(0, subAttrVal.Key4.indexOf('-')));
-                                    //var attrFndVal_subAttrVal = attrFnd_subAttr.SUB_ATTRIBUTE.find()
-                                    //.SUB_ATTRIBUTE.ATTRIBUTE_VALUES.find(attrFndVal_subAttr => attrFndVal_subAttr.ATTRIBUTE_VALUE_LABEL.toUpper() === subAttrVal.Character01)
-                                    //.SELECTED_VALUE = subAttrVal.Character04;
+                                    //console.log(subAttrVal.Key4.substring(0, subAttrVal.Key4.indexOf('-')));
+                                    attrFnd.ATTRIBUTE_VALUES.find(attrFndVal => attrFndVal.ATTRIBUTE_VALUE_LABEL === subAttrVal.Character03)
+                                        .SUB_ATTRIBUTE.find(subAttrID => subAttrID.ATTRIBUTE_ID === subAttrVal.Key4.substring(0, subAttrVal.Key4.indexOf('-')))
+                                        .ATTRIBUTE_VALUES.find(attrFndVal_subAttr => attrFndVal_subAttr.ATTRIBUTE_VALUE_LABEL.toUpper() === subAttrVal.Character01)
+                                        .SELECTED_VALUE = subAttrVal.Character04;
                                 });
 
                                 // attrFnd.ATTRIBUTE_VALUES.map(attrValFnd => {
