df8451ffb7c345b834792af0c8561739dd9d86d9
Bob Schnable <rschnable@saberlogic.com>
2017-12-07 16:56:38 -0500

    RS

--- roles/sales-rep/quote-entry/js/onDataChange.js
+++ roles/sales-rep/quote-entry/js/onDataChange.js
@@ -126,10 +126,12 @@ define(function () {
                                         .SUB_ATTRIBUTE.find(subAttrID => subAttrID.ATTRIBUTE_ID === subAttrVal.Key4.substring(0, subAttrVal.Key4.indexOf('-')))
                                         .SELECTED_VALUE = subAttrVal.Character01;
 
-                                    attrFnd.ATTRIBUTE_VALUES.find(attrFndVal => attrFndVal.ATTRIBUTE_VALUE_LABEL.toUpperCase() === subAttrVal.Character03.toUpperCase())
+                                    var subAttrSelVal = attrFnd.ATTRIBUTE_VALUES.find(attrFndVal => attrFndVal.ATTRIBUTE_VALUE_LABEL.toUpperCase() === subAttrVal.Character03.toUpperCase())
                                         .SUB_ATTRIBUTE.find(subAttrID => subAttrID.ATTRIBUTE_ID === subAttrVal.Key4.substring(0, subAttrVal.Key4.indexOf('-')))
-                                        .ATTRIBUTE_VALUES.find(attrFndVal_subAttr => attrFndVal_subAttr.ATTRIBUTE_VALUE_LABEL.toUpperCase() === subAttrVal.Character01.toUpperCase())
-                                        .SELECTED_VALUE = subAttrVal.Character04;
+                                        .ATTRIBUTE_VALUES.find(attrFndVal_subAttr => attrFndVal_subAttr.ATTRIBUTE_VALUE_LABEL.toUpperCase() === subAttrVal.Character01.toUpperCase());
+                                    if (subAttrSelVal !== undefined) {
+                                        subAttrSelVal.SELECTED_VALUE = subAttrVal.Character04;
+                                    }
                                 });
                             }
 
