1422b2951722e5ab5690866f7737060ea627f111
Bob Schnable <rschnable@saberlogic.com>
2017-12-07 15:10:26 -0500

    RS

--- roles/sales-rep/quote-entry/js/quote.js
+++ roles/sales-rep/quote-entry/js/quote.js
@@ -356,11 +356,6 @@ define(['../../../../libraries/epicor/quote.js'], function (quote_lib) {
     }
 
     function ChangeSubAttribute(bezl, lineNum, attributeID, selectedAttribute, subAttributeID, valueID) {
-        console.log("Attr ID: " + attributeID);
-        console.log("Sel Attr: " + selectedAttribute);
-        console.log("Sub Attr ID: " + subAttributeID);
-        console.log("ValueID: " + valueID);
-        console.log(bezl.data.QuoteDtls);
         bezl.data.QuoteDtls.find(dtl => dtl.QuoteLine === lineNum).Attributes.find(attr => attr.ATTRIBUTE_ID === attributeID)
             .ATTRIBUTE_VALUES.find(attrVal => attrVal.ATTRIBUTE_VALUE === selectedAttribute)
             .SUB_ATTRIBUTE.find(subAttr => subAttr.ATTRIBUTE_ID === subAttributeID).SELECTED_VALUE = valueID;
