c7973a2fc2b8f512b34706d70e5da12d3315a767
Bob Schnable <rschnable@saberlogic.com>
2017-12-07 09:13:38 -0500

    RS

--- roles/sales-rep/quote-entry/js/quote.js
+++ roles/sales-rep/quote-entry/js/quote.js
@@ -397,9 +397,9 @@ define(['../../../../libraries/epicor/quote.js'], function (quote_lib) {
             console.log("Sub Attr Val: " + subAttributeValue)
             console.log("Val ID: " + valueID);
             console.log(bezl.data.QuoteDtls);
-            bezl.data.QuoteDtls.find(attr => attr.ATTRIBUTE_ID === attributeID).ATTRIBUTE_VALUES.find(attrVal => attrVal.ATTRIBUTE_VALUE_LABEL === selectedAttribute)
+            bezl.data.QuoteDtls.find(dtl => dtl.QuoteLine === lineNum).Attributes.find(attr => attr.ATTRIBUTE_ID === attributeID).ATTRIBUTE_VALUES.find(attrVal => attrVal.ATTRIBUTE_VALUE_LABEL === selectedAttribute)
                 .SUB_ATTRIBUTE.find(subAttr => subAttr.ATTRIBUTE_ID === subAttributeID).SELECTED_VALUE = "OTHER";
-            bezl.data.QuoteDtls.find(attr => attr.ATTRIBUTE_ID === attributeID).ATTRIBUTE_VALUES.find(attrVal => attrVal.ATTRIBUTE_VALUE_LABEL === selectedAttribute)
+            bezl.data.QuoteDtls.find(dtl => dtl.QuoteLine === lineNum).Attributes.find(attr => attr.ATTRIBUTE_ID === attributeID).ATTRIBUTE_VALUES.find(attrVal => attrVal.ATTRIBUTE_VALUE_LABEL === selectedAttribute)
                 .SUB_ATTRIBUTE.find(subAttr => subAttr.ATTRIBUTE_ID === subAttributeID).ATTRIBUTE_VALUES.find(subAttrVal => subAttrVal.ATTRIBUTE_VALUE === "OTHER").SELECTED_VALUE = subAttributeValue;
         }
     }
