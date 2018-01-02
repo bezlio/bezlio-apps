073d7bcc72a7f98ba6b9c30b96d271c3798fe9e9
Bob Schnable <rschnable@saberlogic.com>
2018-01-02 13:35:40 -0500

    RS

--- roles/sales-rep/quote-entry/js/quote.js
+++ roles/sales-rep/quote-entry/js/quote.js
@@ -381,10 +381,10 @@ define(['../../../../libraries/epicor/quote.js'], function (quote_lib) {
     function ChangeTypedAttribute(bezl, lineNum, attributeID, selectedAttribute, attributeValue, valueID) {
         if (attributeID.indexOf("MEASURE") === -1) {
             if (valueID === "OTHER") {
-                bezl.data.QuoteDtls.find(dtl => dtl.QuoteLine === lineNum).Attributes.find(attr => attr.ATTRIBUTE_ID === attributeID).SELECTED_VALUE = "OTHER";
-                bezl.data.QuoteDtls.find(dtl => dtl.QuoteLine === lineNum)
-                    .Attributes.find(attr => attr.ATTRIBUTE_ID === attributeID)
-                    .ATTRIBUTE_VALUES.find(val => val.ATTRIBUTE_VALUE === 'OTHER').SELECTED_VALUE = attributeValue;
+                //bezl.data.QuoteDtls.find(dtl => dtl.QuoteLine === lineNum).Attributes.find(attr => attr.ATTRIBUTE_ID === attributeID).SELECTED_VALUE = "OTHER";
+                //bezl.data.QuoteDtls.find(dtl => dtl.QuoteLine === lineNum)
+                //.Attributes.find(attr => attr.ATTRIBUTE_ID === attributeID)
+                //.ATTRIBUTE_VALUES.find(val => val.ATTRIBUTE_VALUE === 'OTHER').SELECTED_VALUE = attributeValue;
             } else {
                 bezl.data.QuoteDtls.find(dtl => dtl.QuoteLine === lineNum).Attributes.find(attr => attr.ATTRIBUTE_ID === attributeID).SELECTED_VALUE = valueID;
                 bezl.data.QuoteDtls.find(dtl => dtl.QuoteLine === lineNum)
