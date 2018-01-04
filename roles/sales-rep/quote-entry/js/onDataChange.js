b987ff01fda17403edd85116d1993afd0b0d13b1
Nick Owens <nowens@saberlogic.com>
2018-01-04 15:10:06 -0500

    instead of displaying data object, turn that assign it to a bezl var, step towards 'show pending' task

--- roles/sales-rep/quote-entry/js/onDataChange.js
+++ roles/sales-rep/quote-entry/js/onDataChange.js
@@ -6,6 +6,7 @@ define(function () {
         }
 
         if (bezl.data.Quotes && !bezl.vars.newQuote && bezl.data.FirstCustomer) {
+            bezl.vars.displayedQuotes = bezl.data.Quotes;
             bezl.vars.loading = false;
         }
 
